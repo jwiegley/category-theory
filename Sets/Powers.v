6aec2c229743f76c31e80b5012c30aa44500901a
John Wiegley <johnw@newartisans.com>
2014-07-09 18:00:38 -0500

    Add things to be proven in Powers.v

---
+++ Sets/Powers.v
@@ -0,0 +1,22 @@
+(A′)′ = A
+∅′ = E
+E′ = ∅
+A ∩ A′ = ∅
+A ∪ A′ = E
+A ⊆ B ↔ B′ ⊆ A′
+(A ∪ B)′ = A′ ∩ B′
+(A ∩ B)′ = A′ ∪ B′
+A - B = A ∩ B′
+A ⊆ B ↔ A - B = ∅
+A - (A - B) = A ∩ B
+A ∩ (B - C) = (A ∩ B) - (A ∩ C)
+A ∩ B ⊆ (A ∩ C) ∪ (B ∩ C′)
+(A ∪ C) ∩ (B ∪ C′) ⊆ A ∪ B
+A + B = (A - B) ∪ (B - A)
+𝒫(Sing A} = {∅, Sing A}
+𝒫({a, b}) = {∅, Sing a, Sing b, {a, b}}
+(Union X)′ = Inter (X′)
+(Inter X)′ = Union (X′)
+Inter (𝒫(X)) = 𝒫(Union X)
+Union (𝒫(X)) ⊆ 𝒫(Union X)
+A ⊆ B → 𝒫(X) ⊆ 𝒫(Y)
