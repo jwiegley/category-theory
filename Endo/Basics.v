1c3f4b4f609f4bc276d4d1b42021faccf44109e3
John Wiegley <johnw@newartisans.com>
2014-07-17 20:46:27 -0500

    Added Maybe.v

--- Endo/Basics.v
+++ Endo/Basics.v
@@ -25,21 +25,31 @@ Ltac ext_eq := (apply ext_eq || apply ext_eqS); intro.
 
 Definition id {X} (a : X) : X := a.
 
+Hint Unfold id.
+
 (* Function composition. *)
 
 Definition compose {A B C} (f : B -> C) (g : A -> B) (x : A) : C := f (g x).
+
+Hint Unfold compose.
 
 Notation "f ∘ g" := (compose f g) (at level 69, right associativity).
 
 Theorem comp_id_left : forall {A B} (f : A -> B), id ∘ f = f.
 Proof. reflexivity. Qed.
 
+Hint Resolve comp_id_left.
+
 Theorem comp_id_right : forall {A B} (f : A -> B), f ∘ id = f.
 Proof. reflexivity. Qed.
+
+Hint Resolve comp_id_right.
 
 Theorem comp_assoc : forall {A B C D} (f : C -> D) (g : B -> C) (h : A -> B),
   f ∘ g ∘ h = (f ∘ g) ∘ h.
 Proof. reflexivity. Qed.
+
+Hint Resolve comp_assoc.
 
 Theorem uncompose : forall {A B C} (f : B -> C) (g : A -> B) (x : A) (y : C),
   (f ∘ g) x = f (g x).
---
+++ Endo/Maybe.v
@@ -0,0 +1,77 @@
+Require Export Monad.
+
+Ltac submatch X :=
+  match X with
+  | match ?Y with _ => _ end => submatch Y
+  | _ => destruct X; auto
+  end.
+
+Ltac simple_solver :=
+  intros;
+  try ext_eq;
+  compute;
+  repeat (
+    match goal with
+    | [ |- context f [match ?X with _ => _ end] ] =>
+      submatch X
+    end);
+  auto.
+
+Section Maybe.
+
+  Variable A : Type.  (* The type which is mapped over. *)
+
+  Inductive Maybe (A : Type): Type :=
+    | Nothing : Maybe A
+    | Just : A -> Maybe A.
+
+
+  (** By registering our simple_solver as the obligation tactics, all of our
+      law obligations will be taken care of for us automatically by the Ltac
+      script above. *)
+  Obligation Tactic := simple_solver.
+
+
+  Definition Maybe_map {X Y} (f : X -> Y) (x : Maybe X) : Maybe Y :=
+    match x with
+    | Nothing => Nothing Y
+    | Just x' => Just Y (f x')
+    end.
+  Hint Unfold Maybe_map.
+
+  Global Program Instance Maybe_Functor : Functor Maybe :=
+  { fmap := @Maybe_map
+  }.
+
+
+  Definition Maybe_apply {X Y} (f : Maybe (X -> Y)) (x : Maybe X) : Maybe Y :=
+    match f with
+    | Nothing => Nothing Y
+    | Just f' => match x with
+      | Nothing => Nothing Y
+      | Just x' => Just Y (f' x')
+      end
+    end.
+  Hint Unfold Maybe_apply.
+
+  Global Program Instance Maybe_Applicative : Applicative Maybe :=
+  { is_functor := Maybe_Functor
+  ; eta := Just
+  ; apply := @Maybe_apply
+  }.
+
+
+  Definition Maybe_join {X} (x : Maybe (Maybe X)) : Maybe X :=
+    match x with
+    | Nothing => Nothing X
+    | Just Nothing => Nothing X
+    | Just (Just x') => Just X x'
+    end.
+  Hint Unfold Maybe_join.
+
+  Global Program Instance Maybe_Monad : Monad Maybe :=
+  { is_applicative := Maybe_Applicative
+  ; mu := @Maybe_join
+  }.
+
+End Maybe.
