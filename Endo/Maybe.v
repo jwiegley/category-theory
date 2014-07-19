6b69d74ff335919a3ab23a1ec37d64c9fee464e1
John Wiegley <johnw@newartisans.com>
2014-07-19 02:08:09 -0500

    Simplify the simple_solver

--- Endo/Maybe.v
+++ Endo/Maybe.v
@@ -1,11 +1,5 @@
 Require Export Monad.
 
-Ltac submatch X :=
-  match X with
-  | match ?Y with _ => _ end => submatch Y
-  | _ => destruct X; auto
-  end.
-
 Ltac simple_solver :=
   intros;
   try ext_eq;
@@ -13,7 +7,7 @@ Ltac simple_solver :=
   repeat (
     match goal with
     | [ |- context f [match ?X with _ => _ end] ] =>
-      submatch X
+      is_var X; destruct X; auto
     end);
   auto.
 
