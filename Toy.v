0fdf3ea044a6b8c176490cd718e5f51a50a9332d
Edward Kmett <ekmett@gmail.com>
2014-08-14 10:05:29 -0400

    work on Toy more accurately 'up to homotopy', Start work on op

--- Toy.v
+++ Toy.v
@@ -6,6 +6,7 @@ Require Export Coq.Program.Tactics.
 Set Implicit Arguments.
 Set Universe Polymorphism.
 Set Printing Projections.
+Set Shrink Obligations.
 (* Set Primitive Projections. *)
 Set Automatic Introduction.
 Set Asymmetric Patterns.
@@ -23,73 +24,7 @@ Delimit Scope path_scope with path.
 
 Local Open Scope hom_scope.
 
-Class Presemicategory :=
-{ ob               :> Type
-; uhom             := Type : Type
-; hom              : ob → ob → uhom where "x ~> y" := (hom x y)
-; compose          : ∀ {x y z}, (y ~> z) → (x ~> y) → (x ~> z) where "f ∘ g" := (compose f g)
-; compose_assoc    : ∀ {w x y z} (f : y ~> z) (g : x ~> y) (h : w ~> x), f ∘ (g ∘ h) = (f ∘ g) ∘ h
-; compose_assoc_op : ∀ {w x y z} (f : y ~> z) (g : x ~> y) (h : w ~> x), (f ∘ g) ∘ h = f ∘ (g ∘ h)
-}.
-
-Bind Scope category_scope with Presemicategory.
-Bind Scope ob_scope with ob.
-Bind Scope hom_scope with hom.
-
-(*
-Create HintDb category discriminated.
-Create HintDb hom discriminated.
-*)
-Arguments ob C%category : rename.
-Arguments hom !C%category x y : rename.
-Arguments compose [!C%category x%ob y%ob z%ob] f%hom g%hom : rename.
-
-Hint Resolve @compose_assoc : category morphism.
-
-Coercion ob : Presemicategory >-> Sortclass.
-
-Infix "~>" := hom : category_scope.
-Infix "~{ C }~>" := (@hom C) (at level 90, right associativity) : category_scope.
-Infix "∘" := compose : hom_scope.
-Open Scope category_scope.
-
-Class Precategory := 
-{ precategory_presemicategory :> Presemicategory
-; id               : ∀ {x}, x ~> x
-; right_id         : ∀ {x y} (f : x ~> y), f ∘ @id x = f
-; left_id          : ∀ {x y} (f : x ~> y), @id y ∘ f = f
-; id_id            : ∀ {x}, @id x ∘ @id x = @id x
-}.
-
-Notation "1" := id : hom_scope.
-
-Arguments id [!C%category] x%ob : rename.
-Arguments right_id [!C%category] x%ob y%ob f%hom : rename.
-Arguments left_id [!C%category] x%ob y%ob f%hom : rename.
-Arguments id_id [!C%category] x%ob : rename.
-
-Coercion precategory_presemicategory : Precategory >-> Presemicategory.
-
-Hint Resolve @left_id @right_id @id_id : category hom.
-Hint Rewrite @left_id @right_id @id_id : category.
-Hint Rewrite @left_id @right_id @id_id : hom.
-
-Class Pregroupoid :=
-{ pregroupoid_precategory :> Precategory
-; inverse : ∀ {x y}, (x ~> y) → (y ~> x)
-}.
-
-Coercion pregroupoid_precategory : Pregroupoid >-> Precategory. 
-
-Program Instance Sets_Presemicategory : Presemicategory :=
-{ ob      := Type
-; hom     := fun x y => x → y
-; compose := fun _ _ _ f g x => f (g x)
-}.
-
-Program Instance Sets_Precategory : Precategory := 
-{ id      := fun _ x => x
-}.
+(** paths *)
 
 Inductive paths {A} : A → A → Type :=
   refl : ∀ (a: A), paths a a.
@@ -110,50 +45,133 @@ Ltac path_induction :=
      end
   ); auto.
 
+(** Presemicategory *)
+
+Class Presemicategory :=
+{ ob               :> Type
+; uhom             := Type : Type
+; hom              : ob → ob → uhom where "x ~> y" := (hom x y)
+; compose          : ∀ {x y z}, (y ~> z) → (x ~> y) → (x ~> z) where "f ∘ g" := (compose f g)
+; compose_assoc    : ∀ {w x y z} (f : y ~> z) (g : x ~> y) (h : w ~> x), f ∘ (g ∘ h) ~ (f ∘ g) ∘ h
+; compose_assoc_op : ∀ {w x y z} (f : y ~> z) (g : x ~> y) (h : w ~> x), (f ∘ g) ∘ h ~ f ∘ (g ∘ h)
+}.
+
+Coercion ob : Presemicategory >-> Sortclass.
+
+Bind Scope category_scope with Presemicategory.
+Bind Scope ob_scope with ob.
+Bind Scope hom_scope with hom.
+
+(*
+Create HintDb category discriminated.
+Create HintDb hom discriminated.
+*)
+Arguments ob C%category : rename.
+Arguments hom !C%category x y : rename.
+Arguments compose [!C%category x%ob y%ob z%ob] f%hom g%hom : rename.
+
+Hint Resolve @compose_assoc : category morphism.
+
+Infix "~>" := hom : category_scope.
+Infix "~{ C }~>" := (@hom C) (at level 90, right associativity) : category_scope.
+Infix "∘" := compose : hom_scope.
+
+Open Scope category_scope.
+
+(** Precategory *)
+
+
+Class Precategory := 
+{ precategory_presemicategory :> Presemicategory
+; id       : ∀ {x}, x ~> x
+; right_id : ∀ {x y} (f : x ~> y), f ∘ @id x ~ f
+; left_id  : ∀ {x y} (f : x ~> y), @id y ∘ f ~ f
+; id_id    : ∀ {x}, @id x ∘ @id x ~ @id x
+}.
+
+Coercion precategory_presemicategory : Precategory >-> Presemicategory.
+
+Notation "1" := id : hom_scope.
+
+Arguments id [!C%category] x%ob : rename.
+Arguments right_id [!C%category] x%ob y%ob f%hom : rename.
+Arguments left_id [!C%category] x%ob y%ob f%hom : rename.
+Arguments id_id [!C%category] x%ob : rename.
+
+Hint Resolve @left_id @right_id @id_id : category hom.
+Hint Rewrite @left_id @right_id @id_id : category.
+Hint Rewrite @left_id @right_id @id_id : hom.
+
+(** Pregroupoid *)
+
+Class Pregroupoid :=
+{ pregroupoid_precategory :> Precategory
+; inverse : ∀ {x y}, (x ~> y) → (y ~> x)
+; inverse_inverse : ∀ {x y} (f : x ~> y), inverse (inverse f) ~ f
+; inverse_left_inverse : ∀ {x y} (f : x ~> y), inverse f ∘ f ~ id x 
+; inverse_right_inverse : ∀ {x y} (f : x ~> y), f ∘ inverse f ~ id y 
+}.
+
+Coercion pregroupoid_precategory : Pregroupoid >-> Precategory. 
+
+Notation "! p" := (inverse p) (at level 50) : hom_scope.
+
+(** Sets *)
+
+Program Instance Sets_Presemicategory : Presemicategory :=
+{ ob      := Type
+; hom     := fun x y => x → y
+; compose := fun _ _ _ f g x => f (g x)
+}.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+  
+Program Instance Sets_Precategory : Precategory := 
+{ precategory_presemicategory := Sets_Presemicategory
+}.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+  
+Definition Sets  := @Sets_Precategory.
+Definition fun_compose := @compose Sets.
+Infix "∘" := fun_compose : type_scope.
+
+
+(** Paths *)
+
 Program Instance Paths_Presemicategory {A} : Presemicategory :=
 { ob := A
 ; hom := @paths A
 }.
-Obligation 1. path_induction. Defined.
-Obligation 2. path_induction. Defined.
-Obligation 3. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
 
 Program Instance Paths_Precategory {A} : Precategory := {
   precategory_presemicategory := @Paths_Presemicategory A
 }.
-Obligation 1. path_induction. Defined.
-Obligation 2. path_induction. Defined.
-Obligation 3. path_induction. Defined.
-     
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+
+
 Program Instance Paths_Pregroupoid {A} : Pregroupoid := {
   pregroupoid_precategory := @Paths_Precategory A
 }.
-Obligation 1. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
+Next Obligation. path_induction. Defined.
  
-Definition Sets  := @Sets_Precategory.
-Definition Paths {A} := @Paths_Pregroupoid A.
-
-Definition fun_compose := @compose Sets.
-Definition path_compose {A} := @compose (@Paths A).
+Definition Paths A := @Paths_Pregroupoid A.
+Definition path_compose {A} := @compose (@Paths_Presemicategory A).
+Definition path_inverse {A} := @inverse (@Paths_Pregroupoid A).
 
 Infix "∘" := path_compose : path_scope.
-Infix "∘" := fun_compose : type_scope.
-
-Notation "! p" := (inverse p) (at level 50) : path_scope.
-
-(* If Prop and HoTT got along, we could Add Parametric Relation (A: Type): A (@paths A) *)
-
-(* paths between paths are homotopies *)
-
-Definition path_left_id {A} {x y : A} (p : x ~ y) : (refl ∘ p ~ p) % path.
-Proof. path_induction. Defined.
-
-Definition path_right_id {A} {x y : A} (p : x ~ y) : (p ∘ refl ~ p) % path. 
-Proof. path_induction. Defined.
-
-Arguments path_left_id [!C%category] x%ob y%ob p%path : rename.
-Arguments path_right_id [!C%category] x%ob y%ob p%path : rename.
-
+Notation "! p" := (path_inverse p) (at level 50) : path_scope.
+ 
 Lemma unique_id {C : Precategory} (id0 id1 : ∀ {x : C}, x ~> x)
   (id1_left  : ∀ (x y : C) (f : x ~> y), @id1 y ∘ f = f)
   (id0_right : ∀ (x y : C) (f : x ~> y), f ∘ @id0 x = f) 
@@ -163,7 +181,7 @@ Proof.
   etransitivity; [ symmetry; apply id1_left | apply id0_right ].
 Qed.
 
-Definition as_left_id { C : Precategory } {x y : C} (f: x ~> y) i (H: i = 1) : i ∘ f = f.
+Definition as_left_id { C : Precategory } {x y : C} (f: x ~> y) i (H: i = 1) : i ∘ f ~ f.
 Proof.
   rewrite -> H.
   path_induction.
@@ -172,7 +190,7 @@ Defined.
 
 Arguments as_left_id [!C%category] x%ob y%ob f%hom i%hom H%hom : rename.
 
-Definition as_right_id { C : Precategory } {x y : C} (f : x ~> y) i (H: i = 1) : f ∘ i = f.
+Definition as_right_id { C : Precategory } {x y : C} (f : x ~> y) i (H: i = 1) : f ∘ i ~ f.
 Proof.    
   rewrite -> H.
   path_induction.
@@ -180,3 +198,57 @@ Proof.
 Defined.
 
 Arguments as_right_id [!C%category] x%ob y%ob f%hom i%hom H%hom : rename.
+
+(* Opposite notions *)
+
+Program Instance Op_Presemicategory (C : Presemicategory) : Presemicategory :=
+{ ob := C
+; hom := fun x y => @hom C y x
+; compose := fun x y z f g => @compose C z y x g f
+}.
+Obligation 1. apply (@compose_assoc_op C). Defined.
+Obligation 2. apply (@compose_assoc C). Defined.
+
+Program Instance Op_Precategory (C : Precategory) : Precategory :=
+{ precategory_presemicategory := Op_Presemicategory C
+; id := @id C
+}.
+Obligation 1. apply (@left_id C). Defined.
+Obligation 2. apply (@right_id C). Defined.
+Obligation 3. apply (@id_id C). Defined.
+
+Program Instance Op_Pregroupoid (C : Pregroupoid) : Pregroupoid :=
+{ pregroupoid_precategory := Op_Precategory C
+; inverse := fun x y => @inverse C y x
+}.
+Obligation 1. apply (@inverse_inverse C). Defined.
+Obligation 2. apply (@inverse_right_inverse C). Defined.
+Obligation 3. apply (@inverse_left_inverse C). Defined.
+
+(* We need funext for these *)
+
+Class Op (T:Type) :=
+{ op : T -> T
+; op_op : ∀ (x : T), op (op x) = x (* needs funext *)
+}.
+
+Program Instance Presemicategory_Op : Op Presemicategory :=
+{ op := Op_Presemicategory
+}.
+Obligation 1. 
+  unfold Op_Presemicategory. 
+  unfold Op_Presemicategory_obligation_1.
+  unfold Op_Presemicategory_obligation_2.
+  simpl.
+  admit. (* funext *)
+Defined.
+
+
+Program Instance Precategory_Op : Op Precategory :=
+{ op := Op_Precategory
+}.
+
+
+Program Instance Pregroupoid_Op : Op Pregroupoid :=
+{ op := Op_Pregroupoid
+}.
