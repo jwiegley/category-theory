d4aad6bf950bc5461ac21a768a37d738e5deddd6
John Wiegley <johnw@newartisans.com>
2017-04-13 18:13:29 -0700

    Restore part of Cat.v

--- Cat.v
+++ Cat.v
@@ -1,4 +1,3 @@
-(* jww (2017-04-13): TODO
 Require Import Lib.
 Require Export Category.
 Require Export Functor.
@@ -9,15 +8,14 @@ Set Universe Polymorphism.
 Set Shrink Obligations.
 Set Implicit Arguments.
 
-Program Instance functor_comp
-  `{Category C} `{Category D} `{Category E}
-  (F : D ⟶ E) (G : C ⟶ D) : C ⟶ E := {
-  fobj := fun x => fobj (fobj x);
-  fmap := fun _ _ f => fmap (fmap f)
-}.
+Program Definition functor_comp
+  `{C : Category} `{D : Category} `{E : Category}
+  (F : D ⟶ E) (G : C ⟶ D) : C ⟶ E :=
+  {| fobj := fun x => fobj (fobj x)
+   ; fmap := fun _ _ f => fmap (fmap f) |}.
 Next Obligation.
-  intros ???.
-  rewrite H2; reflexivity.
+  intros ?? HA.
+  rewrite HA; reflexivity.
 Defined.
 Next Obligation.
   intros.
@@ -30,154 +28,87 @@ Next Obligation.
   reflexivity.
 Qed.
 
-Program Instance Cat : Category Category := {
+Program Instance Cat : Category := {
   hom     := @Functor;
   id      := @Identity;
   compose := @functor_comp
 }.
 Obligation 1.
-  unfold fun_compose.
+  unfold functor_comp.
   destruct f.
-  apply fun_irrelevance.
-  extensionality e.
-  extensionality f.
-  extensionality g.
-  reflexivity.
-Defined.
+  (* jww (2017-04-13): Need to define functor equivalence. *)
+Admitted.
 Obligation 2.
-  unfold fun_compose.
-  destruct f.
-  apply fun_irrelevance.
-  extensionality e.
-  extensionality f.
-  extensionality g.
-  reflexivity.
-Defined.
+  unfold functor_comp.
+  destruct f; simpl.
+Admitted.
 Obligation 3.
-  unfold fun_compose.
-  destruct f.
-  apply fun_irrelevance.
-  extensionality e.
-  extensionality f.
-  reflexivity.
-Defined.
+  unfold functor_comp.
+  destruct f; simpl.
+Admitted.
 
-Lemma fun_id_left `{Category C} `{Category D} `(F : C ⟶ D) :
-  functor_comp Identity F ≈ F.
+Lemma fun_id_left `{C : Category} `{D : Category} `(F : C ⟶ D) :
+  functor_comp Identity F = F.
 Proof.
   destruct F.
-  unfold fun_compose.
+  unfold functor_comp.
   simpl.
-  apply fun_irrelevance.
-  extensionality e.
-  extensionality f.
-  extensionality g.
-  reflexivity.
-Qed.
+Admitted.
 
-Lemma fun_id_right `(F : @Functor C D) : fun_compose F Id = F.
+Lemma fun_id_right `(F : @Functor C D) : functor_comp F Identity = F.
 Proof.
   destruct F.
-  unfold fun_compose.
+  unfold functor_comp.
   simpl.
-  apply fun_irrelevance.
-  extensionality e.
-  extensionality f.
-  extensionality g.
-  reflexivity.
-Qed.
+Admitted.
 
-Program Instance One : Category := {
-    ob      := unit;
-    hom     := fun _ _ => unit;
-    id      := fun _ => tt;
-    compose := fun _ _ _ _ _ => tt
+Program Instance Termi : Category := {
+  ob      := unit;
+  hom     := fun _ _ => unit;
+  id      := fun _ => tt;
+  compose := fun _ _ _ _ _ => tt
 }.
 Obligation 1. destruct f. reflexivity. Qed.
 Obligation 2. destruct f. reflexivity. Qed.
 
-Program Instance Fini `(C : Category) : C ⟶ One := {
-    fobj    := fun _ => tt;
-    fmap    := fun _ _ _ => id
+Program Instance Fini `(C : Category) : C ⟶ Termi := {
+  fobj := fun _ => tt;
+  fmap := fun _ _ _ => id
 }.
 
-Program Instance Zero : Category := {
-    ob      := Empty_set;
-    hom     := fun _ _ => Empty_set
+Program Instance Ini : Category := {
+  ob  := Empty_set;
+  hom := fun _ _ => Empty_set
 }.
-Obligation 3.
-    unfold Zero_obligation_1.
-    unfold Zero_obligation_2.
-    destruct A.
-Defined.
-
-Program Instance Init `(C : Category) : Zero ⟶ C.
-Obligation 1. destruct C. crush. Defined.
-Obligation 2.
-  unfold Init_obligation_1.
-  destruct C. crush.
-Defined.
-Obligation 3.
-  unfold Zero_obligation_1.
-  unfold Init_obligation_1.
-  unfold Init_obligation_2.
-  destruct C. crush.
-Defined.
 Obligation 4.
-  unfold Init_obligation_2.
-  unfold Zero_obligation_2.
-  destruct C. crush.
-Qed.
+  destruct f.
+Defined.
 
-Class HasInitial (C : Category) :=
-{ init_obj    : C
-; init_mor    : ∀ {X}, init_obj ~> X
-; initial_law : ∀ {X} (f g : init_obj ~> X), f = g
-}.
+Program Instance Init `(C : Category) : Ini ⟶ C.
 
-Program Instance Cat_HasInitial : HasInitial Cat := {
-    init_obj := Zero;
-    init_mor := Init
-}.
-Obligation 1.
-  induction f as [F].
-  induction g as [G].
-  assert (F = G).
-    extensionality e.
-    crush.
-  replace F with G. subst.
-  assert (fmap0 = fmap1).
-    extensionality e.
-    extensionality f.
-    extensionality g.
-    crush.
-  apply fun_irrelevance.
-  assumption.
-Qed.
+Obligation 1. destruct H. Defined.
+Obligation 2. destruct f. Defined.
+Obligation 3. destruct X. Qed.
+Obligation 4. destruct f. Qed.
 
-Class HasTerminal (C : Category) :=
-{ term_obj     : C
-; term_mor     : ∀ {X}, X ~> term_obj
-; terminal_law : ∀ {X} (f g : X ~> term_obj), f = g
-}.
+Require Import Terminal.
 
-Program Instance Cat_HasTerminal : HasTerminal Cat := {
-    term_obj := One;
-    term_mor := Fini
+Program Instance Cat_Terminal : @Terminal Cat := {
+  One := Termi;
+  one := Fini
 }.
 Obligation 1.
   destruct f as [F].
   destruct g as [G].
-  assert (F = G).
-    extensionality e.
-    crush.
-  replace F with G. subst.
-  assert (fmap0 = fmap1).
-    extensionality e.
-    extensionality f.
-    extensionality g.
-    crush.
-  apply fun_irrelevance.
-  assumption.
-Qed.
-*)
+Admitted.
+
+Require Import Initial.
+
+Program Instance Cat_Initial : @Initial Cat := {
+  Zero := Ini;
+  zero := Init
+}.
+Obligation 1.
+  induction f as [F].
+  induction g as [G].
+Admitted.
