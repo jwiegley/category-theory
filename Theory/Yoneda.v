2a7ea79d37a8922561ea9c0f2dcc66edcbf2fd64
Jon Sterling <jon@jonmsterling.com>
2017-04-20 11:33:28 -0400

    put canonical yoneda lemma first

--- Theory/Yoneda.v
+++ Theory/Yoneda.v
@@ -17,55 +17,6 @@ Definition Copresheaves (C : Category) := [C, Sets].
 (** This is the Yoneda embedding. *)
 Instance Yoneda `{C : Category} : C ⟶ [C^op, Sets] := CoHomFunctor C.
 
-Program Instance CovariantYonedaLemma `(C : Category) `(F : C ⟶ Sets) {A : C} :
-  [C, Sets] Hom(A,─) F ≅ F A
-:= {
-  to   := {| morphism := fun X => X A id |};
-  from := {| morphism := fun Y : F A =>
-             {| transform := fun X : C =>
-                {| morphism := fun phi : A ~{ C }~> X =>
-                     @fmap C Sets F A X phi Y |} |} |}
-}.
-Next Obligation.
-  repeat intros x y HA.
-  destruct x, y; simpl in *.
-  unfold nat_equiv in HA; simpl in HA.
-  rewrite HA; reflexivity.
-Qed.
-Next Obligation.
-  repeat intros ?? HA.
-  destruct F; simpl in *.
-  apply fmap_respects.
-  apply HA.
-Qed.
-Next Obligation.
-  destruct F; simpl in *.
-  unfold Basics.compose in *.
-  rewrite <- fmap_comp.
-  reflexivity.
-Qed.
-Next Obligation.
-  repeat intro; simpl in *.
-  destruct F; simpl in *.
-  destruct (fmap A A0 x0); simpl.
-  apply proper_morphism.
-  assumption.
-Qed.
-Next Obligation.
-  unfold Basics.compose; simpl.
-  destruct F; simpl in *.
-  rewrite fmap_id.
-  reflexivity.
-Qed.
-Next Obligation.
-  unfold Basics.compose; simpl.
-  destruct F; simpl in *.
-  unfold nat_equiv; simpl; intros.
-  destruct x; simpl in *.
-  unfold Basics.compose in *; simpl.
-  rewrite natural_transformation.
-  apply transform; cat.
-Qed.
 
 Program Instance YonedaLemma `(C : Category) `(F : C^op ⟶ Sets) {A : C} :
 
@@ -118,3 +69,54 @@ Next Obligation.
   rewrite natural_transformation.
   apply transform; cat.
 Qed.
+
+Program Instance CovariantYonedaLemma `(C : Category) `(F : C ⟶ Sets) {A : C} :
+  [C, Sets] Hom(A,─) F ≅ F A
+:= {
+  to   := {| morphism := fun X => X A id |};
+  from := {| morphism := fun Y : F A =>
+             {| transform := fun X : C =>
+                {| morphism := fun phi : A ~{ C }~> X =>
+                     @fmap C Sets F A X phi Y |} |} |}
+}.
+Next Obligation.
+  repeat intros x y HA.
+  destruct x, y; simpl in *.
+  unfold nat_equiv in HA; simpl in HA.
+  rewrite HA; reflexivity.
+Qed.
+Next Obligation.
+  repeat intros ?? HA.
+  destruct F; simpl in *.
+  apply fmap_respects.
+  apply HA.
+Qed.
+Next Obligation.
+  destruct F; simpl in *.
+  unfold Basics.compose in *.
+  rewrite <- fmap_comp.
+  reflexivity.
+Qed.
+Next Obligation.
+  repeat intro; simpl in *.
+  destruct F; simpl in *.
+  destruct (fmap A A0 x0); simpl.
+  apply proper_morphism.
+  assumption.
+Qed.
+Next Obligation.
+  unfold Basics.compose; simpl.
+  destruct F; simpl in *.
+  rewrite fmap_id.
+  reflexivity.
+Qed.
+Next Obligation.
+  unfold Basics.compose; simpl.
+  destruct F; simpl in *.
+  unfold nat_equiv; simpl; intros.
+  destruct x; simpl in *.
+  unfold Basics.compose in *; simpl.
+  rewrite natural_transformation.
+  apply transform; cat.
+Qed.
+
