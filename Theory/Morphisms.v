29426cd7b2c89748c3497265e7d0b4051ce69981
Jon Sterling <jon@jonmsterling.com>
2017-04-20 10:24:54 -0400

    Fix "Ignoring implicit status of product binder...." warning

--- Theory/Morphisms.v
+++ Theory/Morphisms.v
@@ -26,8 +26,8 @@ Class SplitIdempotent {X Y : C} := {
   split_idem_law_2   : split_idem_r ∘ split_idem_s ≈ id
 }.
 
-Definition Epic  `(f : X ~> Y) := ∀ {Z} (g1 g2 : Y ~> Z), g1 ∘ f ≈ g2 ∘ f → g1 ≈ g2.
-Definition Monic `(f : X ~> Y) := ∀ {Z} (g1 g2 : Z ~> X), f ∘ g1 ≈ f ∘ g2 → g1 ≈ g2.
+Definition Epic  `(f : X ~> Y) := ∀ Z (g1 g2 : Y ~> Z), g1 ∘ f ≈ g2 ∘ f → g1 ≈ g2.
+Definition Monic `(f : X ~> Y) := ∀ Z (g1 g2 : Z ~> X), f ∘ g1 ≈ f ∘ g2 → g1 ≈ g2.
 
 Definition Bimorphic `(f : X ~> Y) := (Epic f * Monic f)%type.
 Definition SplitEpi  `(f : X ~> Y) := Retraction f.
