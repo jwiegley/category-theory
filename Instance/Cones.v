aaecfd6199c64548459d773fe821aa925df9d4e7
John Wiegley <johnw@newartisans.com>
2017-04-23 12:56:49 -0700

    Add Instance/Cones.v, with just a comment for now

---
+++ Instance/Cones.v
@@ -0,0 +1,9 @@
+(* Wikipedia: "We can define the category of cones to F as the comma category
+  (Δ ↓ F). Morphisms of cones are then just morphisms in this category. This
+  equivalence is rooted in the observation that a natural map between constant
+  functors Δ(N), Δ(M) corresponds to a morphism between N and M. In this
+  sense, the diagonal functor acts trivially on arrows. In similar vein,
+  writing down the definition of a natural map from a constant functor Δ(N) to
+  F yields the same diagram as the above. As one might expect, a morphism from
+  a cone (N, ψ) to a cone (L, φ) is just a morphism N → L such that all the
+  "obvious" diagrams commute (see the first diagram in the next section)." *)
