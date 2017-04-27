ca4d7786fb8700983b467bf7ed45ceb3650845ea
John Wiegley <johnw@newartisans.com>
2017-04-27 13:16:44 -0700

    Add monoidal functors, of various flavors

--- Structure/Monoidal.v
+++ Structure/Monoidal.v
@@ -29,3 +29,51 @@ Class Monoidal := {
 }.
 
 End Monoidal.
+
+Infix "⨂" := (@tensor _ _) (at level 30, right associativity).
+
+Section MonoidalFunctor.
+
+Context `{C : Category}.
+Context `{D : Category}.
+Context `{@Monoidal C}.
+Context `{@Monoidal D}.
+Context `{F : C ⟶ D}.
+
+Class LaxMonoidalFunctor := {
+  pure : munit ~> F munit;
+  ap {X Y} : F X ⨂ F Y ~> F (X ⨂ Y)
+}.
+
+Class StrongMonoidalFunctor := {
+  pure_iso : munit ≅ F munit;
+  ap_iso {X Y} : F X ⨂ F Y ≅ F (X ⨂ Y)
+}.
+
+Definition StrongMonoidalFunctor_Is_Lax (S : StrongMonoidalFunctor) :
+  LaxMonoidalFunctor := {|
+  pure := to (@pure_iso S);
+  ap := fun X Y => to (@ap_iso S X Y)
+|}.
+
+End MonoidalFunctor.
+
+Class StrongFunctor `{C : Category} `{@Monoidal C} (F : C ⟶ C) := {
+  strength {X Y} : X ⨂ F Y ≅ F (X ⨂ Y)
+}.
+
+Class RightStrongFunctor `{C : Category} `{@Monoidal C} (F : C ⟶ C) := {
+  right_strength {X Y} : F X ⨂ Y ≅ F (X ⨂ Y)
+}.
+
+(* A strong lax monoidal functor is called an "applicative functor" in
+   Haskell. *)
+
+Set Warnings "-non-primitive-record".
+
+Class StrongLaxMonoidalFunctor
+      `{C : Category}
+      `{@Monoidal C}
+      (F : C ⟶ C)
+      `{@StrongFunctor _ _ F}
+      `{@LaxMonoidalFunctor _ _ _ _ F}.
