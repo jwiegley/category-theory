46dc4ffd22bdf0f8d5132003b67ed0db7d734874
John Wiegley <johnw@newartisans.com>
2017-04-27 02:18:46 -0700

    Add the Constant functor

---
+++ Functor/Constant.v
@@ -0,0 +1,13 @@
+Set Warnings "-notation-overridden".
+
+Require Import Category.Lib.
+Require Export Category.Theory.Functor.
+
+Generalizable All Variables.
+Set Primitive Projections.
+Set Universe Polymorphism.
+
+Program Instance Constant `(C : Category) `{D : Category} (x : D) : C ⟶ D := {
+  fobj := fun _ => x;
+  fmap := fun _ _ _ => id
+}.
