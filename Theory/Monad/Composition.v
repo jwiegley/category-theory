f319a7fb210c6bfadbb9f70e4ddb5967d54f97c2
John Wiegley <johnw@newartisans.com>
2017-05-05 01:54:15 -0700

    Monads compose with strong lax monoidal functors given weak distributivity

--- Theory/Monad/Composition.v
+++ Theory/Monad/Composition.v
@@ -1,32 +1,107 @@
 Set Warnings "-notation-overridden".
 
-(*
-(*
-Class Monad_Distributes `{M : Monad} `{N : Applicative} := {
-  prod : forall A, N (M (N A)) -> M (N A)
-}.
+Require Import Category.Lib.
+Require Export Category.Theory.Monad.
+Require Export Category.Structure.Monoidal.
+Require Export Category.Functor.Monoidal.
+Require Export Category.Isomorphism.Monoid.Monad.
 
-Arguments prod M {_} N {_ Monad_Distributes} A _.
-*)
+Generalizable All Variables.
+Set Primitive Projections.
+Set Universe Polymorphism.
+Unset Transparent Obligations.
+
+Section MonadComposition.
+
+Context `{C : Category}.
+Context `{@Monoidal C}.
+
+Context `{M : C ⟶ C}.
+Context `{O : @Monad C M}.
+
+Context `{N : C ⟶ C}.
+Context `{@StrongFunctor C _ N}.
+Context `{@LaxMonoidalFunctor C C _ _ N}.
+
+Class Monad_Distributes := {
+  prod {A} : N (M (N A)) ~> M (N A);
+
+  prod_fmap_fmap {A B} (f : A ~> B) :
+    @prod B ∘ fmap[N] (fmap[M ○ N] f) ≈ fmap[M ○ N] f ∘ @prod A;
+  prod_pure {A} : @prod A ∘ pure[N] ≈ id;
+  prod_fmap_pure {A} : @prod A ∘ fmap[N] (ret[M] ∘ pure[N]) ≈ ret[M];
+  prod_fmap_join_fmap_prod {A} :
+    @prod A ∘ fmap[N] (join[M] ∘ fmap[M] (@prod A))
+      ≈ join[M] ∘ fmap[M] (@prod A) ∘ @prod (M (N A))
+}.
 
 (* These proofs are due to Mark P. Jones and Luc Duponcheel in their article
    "Composing monads", Research Report YALEU/DCS/RR-1004, December 1993.
 
-   Given any Monad M, and any Premonad N (i.e., having pure), and further given
-   an operation [prod] and its accompanying four laws, it can be shown that M
-   N is closed under composition.
-*)
-Class Monad_DistributesLaws `{Applicative (M ∘ N)} `{Monad_Distributes M N} :=
-{
-  m_monad_laws :> MonadLaws M;
-  n_applicative_laws :> ApplicativeLaws N;
+   Given any Monad M, and any Strong LaxMonoidalFunctor N, and further given
+   that M distributes over N according to Monad_Distributes, it can be shown
+   that M ○ N is always a Monad. *)
 
-  prod_fmap_fmap : forall A B (f : A -> B),
-    prod M N B ∘ fmap[N] (fmap[M ∘ N] f) ≈ fmap[M ∘ N] f ∘ prod M N A;
-  prod_pure : forall A, prod M N A ∘ pure[N] ≈ @id (M (N A));
-  prod_fmap_pure : forall A, prod M N A ∘ fmap[N] (pure[M ∘ N]) ≈ pure[M];
-  prod_fmap_join_fmap_prod : forall A,
-    prod M N A ∘ fmap[N] (join[M] ∘ fmap[M] (prod M N A))
-      ≈ join[M] ∘ fmap[M] (prod M N A) ∘ prod M N (M (N A))
+Local Obligation Tactic := idtac.
+
+Global Program Instance Monad_Composition `{Monad_Distributes} :
+  @Monad _ (M ○ N) := {
+  ret  := fun _ => ret[M] ∘ pure[N];
+  join := fun _ => join[M] ∘ fmap[M] prod
 }.
-*)
+Next Obligation.
+  simpl; intros.
+  rewrite comp_assoc.
+  rewrite <- fmap_ret.
+  rewrite <- !comp_assoc.
+  rewrite <- fmap_pure.
+  reflexivity.
+Qed.
+Next Obligation.
+  simpl; intros.
+  rewrite <- comp_assoc with (f := join[M]).
+  rewrite <- comp_assoc with (f := join[M]).
+  rewrite comp_assoc with (f := fmap[M] prod).
+  rewrite <- join_fmap_fmap.
+  rewrite comp_assoc.
+  rewrite comp_assoc with (f := join[M]).
+  rewrite comp_assoc with (f := join[M]).
+  rewrite <- join_fmap_join.
+  rewrite <- !comp_assoc.
+  rewrite <- !fmap_comp.
+  rewrite comp_assoc.
+  rewrite <- prod_fmap_join_fmap_prod.
+  reflexivity.
+Qed.
+Next Obligation.
+  simpl; intros.
+  rewrite <- join_fmap_ret.
+  rewrite <- !comp_assoc.
+  rewrite <- fmap_comp.
+  rewrite prod_fmap_pure.
+  reflexivity.
+Qed.
+Next Obligation.
+  simpl; intros.
+  rewrite <- prod_pure.
+  rewrite !comp_assoc.
+  apply compose_respects; [|reflexivity].
+  rewrite <- !comp_assoc.
+  rewrite <- fmap_ret.
+  rewrite comp_assoc.
+  rewrite join_ret; cat.
+Qed.
+Next Obligation.
+  simpl; intros.
+  rewrite comp_assoc.
+  rewrite <- join_fmap_fmap.
+  rewrite <- comp_assoc.
+  rewrite <- fmap_comp.
+  pose proof (@prod_fmap_fmap _ a).
+  simpl in X; rewrite X.
+  rewrite fmap_comp.
+  rewrite comp_assoc.
+  reflexivity.
+Qed.
+
+End MonadComposition.
