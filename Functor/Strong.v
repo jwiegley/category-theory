8e15fc48704f48605f4677afd7603fc90f133d37
John Wiegley <johnw@newartisans.com>
2017-05-08 00:02:40 -0700

    Prove that the product of strong functors is strong, and vice versa

--- Functor/Strong.v
+++ Functor/Strong.v
@@ -62,21 +62,19 @@ Class RightStrongFunctor `{C : Category} `{@Monoidal C} (F : C ⟶ C) := {
       ≈ fmap[F] (from (@tensor_assoc _ _ X Y Z)) ∘ rstrength
 }.
 
+Require Import Category.Functor.Product.
+
 Section Strong.
 
 Context `{C : Category}.
 Context `{@Monoidal C}.
 Context `{F : C ⟶ C}.
 
-Global Program Instance Id_Strong : StrongFunctor Id[C] := {
+Global Program Instance Id_StrongFunctor : StrongFunctor Id[C] := {
   strength_nat := {| transform := fun p => _ |}
 }.
-Next Obligation.
-  exact id.
-Defined.
-Next Obligation.
-  unfold bimap; cat.
-Qed.
+Next Obligation. exact id. Defined.
+Next Obligation. unfold bimap; cat. Qed.
 
 Local Obligation Tactic := program_simpl.
 
@@ -135,6 +133,106 @@ Next Obligation.
   apply compose_respects; [|reflexivity].
   apply fmap_respects.
   split; simpl; cat.
+Qed.
+
+Theorem ProductFunctor_Strong (G : C ⟶ C) :
+  (@StrongFunctor C _ F * @StrongFunctor C _ G) <-->
+  (@StrongFunctor (C ∏ C) _ (F ∏⟶ G)).
+Proof.
+  split; intros.
+  { unshelve econstructor.
+    destruct X as [O P].
+    - natural; simplify; intros;
+      simplify; simpl in *; simplify; simpl in *.
+      + exact (transform[@strength_nat _ _ _ O] (x1, x0)).
+      + exact (transform[@strength_nat _ _ _ P] (y, y0)).
+      + destruct O, strength_nat0; simpl in *.
+        abstract apply (natural_transformation (x5, x4)).
+      + destruct P, strength_nat0; simpl in *.
+        abstract apply (natural_transformation (y3, y4)).
+    - intros.
+      destruct X, X0; simpl.
+      split; abstract apply strength_id_left.
+    - intros.
+      destruct X, X0, Y, Z; simpl.
+      split; abstract apply strength_assoc.
+  }
+  { split.
+    { destruct X.
+      unshelve econstructor.
+      - natural; simplify; simpl; intros; simplify.
+        + exact (fst (transform[strength_nat0] ((x, I), (y, I)))).
+        + simpl in *.
+          abstract exact (fst (@natural_transformation
+                                 _ _ _ _ strength_nat0
+                                 (x1, I, (y1, I)) (x0, I, (y0, I))
+                                 ((x, id), (y, id)))).
+      - simpl; intros.
+        abstract exact (fst (strength_id_left0 (X, I))).
+      - simpl; intros.
+        pose proof (@unit_left C H I) as X0.
+        pose proof (fst (@natural_transformation _ _ _ _ strength_nat0
+                           (X, I, (Y ⨂ Z, I))
+                           (X, I, (Y ⨂ Z, I ⨂ I))
+                           ((id[X], id[I]), (id[Y ⨂ Z], from X0)))) as X1.
+        simpl in X1.
+        rewrite bimap_id_id in X1.
+        rewrite !fmap_id in X1.
+        rewrite id_left in X1.
+        rewrite bimap_id_id in X1.
+        rewrite id_right in X1.
+        rewrite X1; clear X1.
+        pose proof (fst (@natural_transformation _ _ _ _ strength_nat0
+                           (X ⨂ Y, I, (Z, I))
+                           (X ⨂ Y, I ⨂ I, (Z, I))
+                           ((id[X ⨂ Y], from X0), (id[Z], id[I])))) as X1.
+        simpl in X1.
+        rewrite bimap_id_id in X1.
+        rewrite !fmap_id in X1.
+        rewrite id_left in X1.
+        rewrite bimap_id_id in X1.
+        rewrite id_right in X1.
+        rewrite X1; clear X1.
+        abstract exact (fst (strength_assoc0 (X, I) (Y, I) (Z, I))).
+    }
+    { destruct X.
+      unshelve econstructor.
+      - natural; simplify; simpl; intros; simplify.
+        + exact (snd (transform[strength_nat0] ((I, x), (I, y)))).
+        + simpl in *.
+          abstract exact (snd (@natural_transformation
+                                 _ _ _ _ strength_nat0
+                                 (I, x1, (I, y1)) (I, x0, (I, y0))
+                                 ((id, x), (id, y)))).
+      - simpl; intros.
+        abstract exact (snd (strength_id_left0 (I, X))).
+      - simpl; intros.
+        pose proof (@unit_left C H I) as X0.
+        pose proof (snd (@natural_transformation _ _ _ _ strength_nat0
+                           (I, X, (I, Y ⨂ Z))
+                           (I, X, (I ⨂ I, Y ⨂ Z))
+                           ((id[I], id[X]), (from X0, id[Y ⨂ Z])))) as X1.
+        simpl in X1.
+        rewrite bimap_id_id in X1.
+        rewrite !fmap_id in X1.
+        rewrite id_left in X1.
+        rewrite bimap_id_id in X1.
+        rewrite id_right in X1.
+        rewrite X1; clear X1.
+        pose proof (snd (@natural_transformation _ _ _ _ strength_nat0
+                           (I, X ⨂ Y, (I, Z))
+                           (I ⨂ I, X ⨂ Y, (I, Z))
+                           ((from X0, id[X ⨂ Y]), (id[I], id[Z])))) as X1.
+        simpl in X1.
+        rewrite bimap_id_id in X1.
+        rewrite !fmap_id in X1.
+        rewrite id_left in X1.
+        rewrite bimap_id_id in X1.
+        rewrite id_right in X1.
+        rewrite X1; clear X1.
+        abstract exact (snd (strength_assoc0 (I, X) (I, Y) (I, Z))).
+    }
+  }
 Qed.
 
 End Strong.
