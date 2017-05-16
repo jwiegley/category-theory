f84a5a3132e1e464c27aada674c4b6d3a3a533bd
John Wiegley <johnw@newartisans.com>
2017-05-15 23:13:44 -0400

    Add note about moving Applicative to symmetric closed monoidal

--- Functor/Strong/Product.v
+++ Functor/Strong/Product.v
@@ -32,14 +32,14 @@ Program Definition Product_Strong :
 |}.
 Next Obligation.
   simpl; intros.
-  exact (bimap strength strength ∘ braid2 ∘ bimap diagonal id).
+  exact (strength ⨂ strength ∘ twist2 ∘ ∆X ⨂ id).
 Defined.
 Next Obligation.
   simpl; intros.
   unfold Product_Strong_obligation_1.
   pose proof (@strength_natural _ _ _ O _ _ g _ _ h) as X0.
   pose proof (@strength_natural _ _ _ P _ _ g _ _ h) as X1.
-  pose proof (braid2_natural _ _ g _ _ g _ _ (fmap[F] h) _ _ (fmap[G] h)) as X2.
+  pose proof (twist2_natural _ _ g _ _ g _ _ (fmap[F] h) _ _ (fmap[G] h)) as X2.
   simpl in *; normal.
   rewrite X0; clear X0.
   rewrite X1; clear X1.
@@ -49,10 +49,59 @@ Next Obligation.
   rewrite (comp_assoc (bimap (bimap _ _) _)).
   rewrite X2; clear X2.
   normal.
-  rewrite diagonal_natural.
+  srewrite diagonal_natural.
   reflexivity.
 Qed.
 Next Obligation.
+  intros.
+  simpl; intros.
+  unfold Product_Strong_obligation_1.
+  pose proof (@strength_id_left _ _ _ O) as X0.
+  pose proof (@strength_id_left _ _ _ P) as X1.
+  normal.
+  rewrite X0, X1; clear X0 X1.
+  unfold twist2.
+  rewrite !comp_assoc.
+  rewrite <- bimap_id_left_right.
+  rewrite <- !comp_assoc.
+  rewrite (comp_assoc _ (tensor_assoc⁻¹)).
+  rewrite <- bimap_triangle_left.
+  rewrite (comp_assoc unit_left).
+  rewrite <- to_unit_left_natural.
+  normal.
+  rewrite <- triangle_identity.
+  normal.
+  rewrite unit_right_twist.
+  rewrite <- bimap_triangle_left.
+  rewrite to_unit_left_natural.
+  rewrite inverse_triangle_identity.
+  rewrite <- !comp_assoc.
+  rewrite (comp_assoc (tensor_assoc⁻¹)).
+  rewrite iso_from_to; normal.
+  rewrite (@unit_terminal _ _ _ _ _ id); cat.
+Qed.
+Next Obligation.
+  intros.
+  simpl; intros.
+  unfold Product_Strong_obligation_1.
+  pose proof (@strength_assoc _ _ _ O) as X0.
+  pose proof (@strength_assoc _ _ _ P) as X1.
+  normal.
+  rewrite X0, X1; clear X0 X1.
+  rewrite !bimap_comp.
+  rewrite <- !comp_assoc.
+  apply compose_respects; [reflexivity|].
+  normal.
+  unfold twist2.
+  normal.
+  apply (iso_monic tensor_assoc).
+  rewrite !comp_assoc.
+  rewrite iso_to_from, id_left.
+  rewrite bimap_comp.
+  rewrite comp_assoc.
+  rewrite <- to_tensor_assoc_natural.
+  rewrite <- bimap_id_left_right.
+  normal.
 Admitted.
 *)
 
