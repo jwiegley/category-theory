1819bdbd13bda33410d8d117a12ccc60dffc8ea0
John Wiegley <johnw@newartisans.com>
2017-05-17 22:57:38 -0400

    Define Wedges

---
+++ Theory/Wedge.v
@@ -0,0 +1,34 @@
+Set Warnings "-notation-overridden".
+
+Require Import Category.Lib.
+Require Export Category.Theory.Functor.
+Require Export Category.Functor.Bifunctor.
+Require Export Category.Construction.Opposite.
+Require Export Category.Construction.Product.
+
+Generalizable All Variables.
+Set Primitive Projections.
+Set Universe Polymorphism.
+Unset Transparent Obligations.
+
+Section Wedge.
+
+Context `{C : Category}.
+Context `{D : Category}.
+Context `{F : C^op ∏ C ⟶ D}.
+
+Class Wedge := {
+  wedge : D;
+
+  wedge_map {X : C} : wedge ~{D}~> F (X, X);
+
+  ump_wedges {X Y : C} (f : X ~{C}~> Y) :
+    bimap[F] id f ∘ wedge_map ≈ bimap[F] (op f) id ∘ wedge_map
+}.
+
+End Wedge.
+
+Coercion wedge : Wedge >-> ob.
+
+Notation "wedge_map[ C ]" := (@wedge_map _ _ _ C _)
+  (at level 9, format "wedge_map[ C ]") : category_scope.
