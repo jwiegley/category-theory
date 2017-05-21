deb5370eb8bec735e63c4dc069ce5484a780efc2
John Wiegley <johnw@newartisans.com>
2017-05-20 23:27:52 -0400

    Drop some unneeded code in Isomorphism/Comma/Product.v

--- Isomorphism/Comma/Product.v
+++ Isomorphism/Comma/Product.v
@@ -29,41 +29,9 @@ Ltac simpl_cat :=
     | _ => cat
     end).
 
-Theorem Comma_Product' `{C : Category} `{D : Category}
-        (F : C ⟶ 1) (G : D ⟶ 1) :
-  (F ↓ G) ≅[Cat] C ∏ D.
-Proof. Time simpl_cat. Time Qed.
-
 Theorem Comma_Product `{C : Category} `{D : Category}
         (F : C ⟶ 1) (G : D ⟶ 1) :
   (F ↓ G) ≅[Cat] C ∏ D.
-Proof.
-  isomorphism; simpl.
-  - functor; abstract cat.
-  - functor.
-    + simpl; intros; cat.
-    + simpl; intros; cat.
-    + abstract proper.
-    + abstract cat.
-    + abstract cat.
-  - isomorphism.
-    + transform.
-      * simpl; intros; cat.
-      * abstract cat.
-    + transform.
-      * simpl; intros; cat.
-      * abstract cat.
-    + simpl; abstract cat.
-    + simpl; abstract cat.
-  - isomorphism.
-    + transform.
-      * simpl; intros; cat.
-      * abstract cat.
-    + transform.
-      * simpl; intros; cat.
-      * abstract cat.
-    + simpl; abstract cat.
-    + simpl; abstract cat.
-Time Qed.
+Proof. simpl_cat. Qed.
 
-Time End ProductComma.
+End ProductComma.
