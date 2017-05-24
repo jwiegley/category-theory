c907d0811a029bd7effe7e0fab123fb4b66e272a
John Wiegley <johnw@newartisans.com>
2017-05-24 16:58:19 -0400

    Define Complete and Cocomplete

---
+++ Structure/Complete.v
@@ -0,0 +1,15 @@
+Set Warnings "-notation-overridden".
+
+Require Import Category.Lib.
+Require Export Category.Structure.Limit.
+
+Generalizable All Variables.
+Set Primitive Projections.
+Set Universe Polymorphism.
+Unset Transparent Obligations.
+
+Definition Complete {C : Category} :=
+  ∀ (D : Category) (F : D ⟶ C), Limit F.
+
+Definition Cocomplete {C : Category} :=
+  ∀ (D : Category) (F : D ⟶ C), Colimit F.
