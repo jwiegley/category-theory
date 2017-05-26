e62d77bce0d1499e3b5af058f52b919537396fe4
John Wiegley <johnw@newartisans.com>
2017-05-26 11:29:59 -0700

    Simplify the definition of the category _2

--- Instance/Two.v
+++ Instance/Two.v
@@ -8,69 +8,100 @@ Set Primitive Projections.
 Set Universe Polymorphism.
 Unset Transparent Obligations.
 
-Local Ltac crush :=
-  repeat (
-    match goal with
-    | [ H : _ ∧ _ |- _ ] => destruct H
-    | [ H : _ ∨ _ |- _ ] => destruct H
-    end; subst; auto).
+Inductive TwoObj : Type := TwoX | TwoY.
+
+Inductive TwoHom : TwoObj -> TwoObj -> Type :=
+  | TwoIdX : TwoHom TwoX TwoX
+  | TwoIdY : TwoHom TwoY TwoY
+  | TwoXY  : TwoHom TwoX TwoY.
+
+Definition caseTwoXX (p : TwoHom TwoX TwoX) :
+  forall
+    (P : TwoHom TwoX TwoX -> Type)
+    (PTwoIdX : P TwoIdX), P p :=
+  match p with
+  | TwoIdX => fun _ P => P
+  end.
+
+Definition caseTwoYY (p : TwoHom TwoY TwoY) :
+  forall
+    (P : TwoHom TwoY TwoY -> Type)
+    (PTwoIdY : P TwoIdY), P p :=
+  match p with
+  | TwoIdY => fun _ P => P
+  end.
+
+Definition caseTwoXY (p : TwoHom TwoX TwoY) :
+  forall
+    (P : TwoHom TwoX TwoY -> Type)
+    (PTwoIdY : P TwoXY), P p :=
+  match p with
+  | TwoXY => fun _ P => P
+  end.
+
+Lemma TwoHom_Y_X_absurd : TwoHom TwoY TwoX -> False.
+Proof. inversion 1. Qed.
+
+Local Hint Extern 4 =>
+  match goal with
+    [ H : TwoHom TwoY TwoX |- _ ] =>
+    contradiction (TwoHom_Y_X_absurd H)
+  end : two_laws.
 
 (* The category 2 has two objects, their identity morphisms, and one morphism
    from the first object to the second (here denoted false and true). *)
 
 Program Definition _2 : Category := {|
-  ob      := bool;
-  hom     := fun x y => ((x = y) + ((x = false) * (y = true)))%type;
-  homset  := fun x y => {| equiv := fun f g => f = g |};
-  id      := fun x => Datatypes.inl (eq_refl x);
-  compose := fun _ _ _ f g =>
-    match f with
-    | Datatypes.inl H =>
-      match g with
-      | Datatypes.inl H0 => Datatypes.inl (eq_trans H0 H)
-      | Datatypes.inr H0 =>
-        Datatypes.inr
-          match H0 with
-          | pair H1 H2 => pair H1 (eq_trans (eq_sym H) H2)
-          end
-      end
-    | Datatypes.inr H =>
-      Datatypes.inr
-        match g with
-        | Datatypes.inl H0 =>
-          match H with
-          | pair H1 H2 => pair (eq_trans H0 H1) H2
-          end
-        | Datatypes.inr H0 =>
-          match H with
-          | pair _ H2 =>
-            match H0 with
-            | pair H3 _ => pair H3 H2
-            end
-          end
-        end
+  ob      := TwoObj;
+  hom     := TwoHom;
+  homset  := fun x y => {| equiv := eq |};
+  id      := fun x => match x with
+    | TwoX => TwoIdX
+    | TwoY => TwoIdY
+    end;
+  compose := fun x y z (f : TwoHom y z) (g : TwoHom x y) =>
+    match x, y, z with
+    | TwoX, TwoX, TwoX => TwoIdX
+    | TwoY, TwoY, TwoY => TwoIdY
+    | TwoX, TwoX, TwoY => TwoXY
+    | TwoX, TwoY, TwoY => TwoXY
+    | _,    _,    _    => _
     end
 |}.
+Next Obligation. destruct x, y, z; intuition. Qed.
 Next Obligation.
-  destruct f.
-    reflexivity.
-  destruct p; subst.
-  reflexivity.
+  destruct x, y, z; auto with two_laws;
+  intuition; discriminate.
 Qed.
 Next Obligation.
-  destruct f.
-    subst; simpl.
-    reflexivity.
-  destruct p; subst.
-  reflexivity.
+  destruct x, y, z; auto with two_laws;
+  intuition; discriminate.
 Qed.
 Next Obligation.
-  destruct f, g, h;
-  subst; simpl; intuition;
-  subst; simpl; auto.
+  destruct x, y, z; auto with two_laws;
+  intuition; discriminate.
 Qed.
 Next Obligation.
-  destruct f, g, h;
-  subst; simpl; intuition;
-  subst; simpl; auto.
+  destruct X, Y; auto with two_laws; intuition.
+  - pattern f.
+    apply caseTwoXX; reflexivity.
+  - pattern f.
+    apply caseTwoXY; reflexivity.
+  - pattern f.
+    apply caseTwoYY; reflexivity.
+Qed.
+Next Obligation.
+  destruct X, Y; auto with two_laws; intuition.
+  - pattern f.
+    apply caseTwoXX; reflexivity.
+  - pattern f.
+    apply caseTwoXY; reflexivity.
+  - pattern f.
+    apply caseTwoYY; reflexivity.
+Qed.
+Next Obligation.
+  destruct X, Y, Z, W; auto with two_laws; intuition.
+Qed.
+Next Obligation.
+  destruct X, Y, Z, W; auto with two_laws; intuition.
 Qed.
