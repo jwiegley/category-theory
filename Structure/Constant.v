8ce6fed3d24f6c8490831d63dc0327fe99b47413
John Wiegley <johnw@newartisans.com>
2017-05-27 20:53:48 -0700

    Add a comment about the Constant structure

--- Structure/Constant.v
+++ Structure/Constant.v
@@ -13,6 +13,9 @@ Section Constant.
 Context {C : Category}.
 Context `{@Terminal C}.
 
+(* This structure represents "embedded constants", by way of an object that is
+   able to host foreign values of a given Coq type. *)
+
 Class Constant (A : Type) := {
   Const : A -> ob;
   constant (x : A) : One ~{C}~> Const x
