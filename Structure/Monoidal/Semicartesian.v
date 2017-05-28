268b9ab2fde476171151635b87ea7d8642deb55d
John Wiegley <johnw@newartisans.com>
2017-05-28 14:47:09 -0700

    Show that semicartesian monoidal categories take the unit as terminal

--- Structure/Monoidal/Semicartesian.v
+++ Structure/Monoidal/Semicartesian.v
@@ -12,7 +12,7 @@ Set Primitive Projections.
 Set Universe Polymorphism.
 Unset Transparent Obligations.
 
-Section Monoidal.
+Section SemicartesianMonoidal.
 
 Context {C : Category}.
 
@@ -32,7 +32,7 @@ Corollary eliminate_comp `{@Monoidal C} `{@SemicartesianMonoidal _} `{f : A ~> B
   eliminate ∘ f ≈ eliminate.
 Proof. intros; apply unit_terminal. Qed.
 
-End Monoidal.
+End SemicartesianMonoidal.
 
 Require Import Category.Structure.Terminal.
 
@@ -45,3 +45,21 @@ Program Definition SemicartesianMonoidal_Terminal `{@Monoidal C}
   one := @eliminate _ _ _;
   one_unique := @unit_terminal _ _ _
 |}.
+
+Import EqNotations.
+
+(* Likewise, any monoidal category whose terminal object is the unit object,
+   is semicartesian monoidal. *)
+
+Program Definition Terminal_SemicartesianMonoidal `{M : @Monoidal C}
+        `{T : @Terminal C} (Heq : One = @I C M) :
+  @SemicartesianMonoidal C _ := {|
+  eliminate := fun X => rew Heq in one;
+  unit_terminal := fun X f g =>
+    _ (one_unique (rew <- Heq in f) (rew <- Heq in g))
+|}.
+Next Obligation.
+  unfold eq_rect_r, eq_rect, eq_sym in x.
+  destruct Heq.
+  assumption.
+Defined.
