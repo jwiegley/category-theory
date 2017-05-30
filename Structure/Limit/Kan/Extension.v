48d1f837b0e5fe063e301f69310b9e20c8ab294f
John Wiegley <johnw@newartisans.com>
2017-05-30 12:18:22 -0700

    Further note is Structure/Limit/Kan/Extension.v

--- Structure/Limit/Kan/Extension.v
+++ Structure/Limit/Kan/Extension.v
@@ -40,7 +40,7 @@ Proof.
   - simpl.
     pose proof (iso_to_from
                   ((@adj _ _ _ _ ran_adjoint
-                         (Ran (Erase J) F) F)) nat_identity tt).
+                         (Ran (Erase J) F) F)) nat_identity tt) as X;
     simpl in X.
     rewrite fmap_id in X.
     rewrite <- X.
@@ -56,56 +56,38 @@ Proof.
 
     pose proof (@to_adj_nat_l _ _ _ _ ran_adjoint
                               (Ran (Erase J) F) (Const (Lim F))
-                              F nat from_ran tt).
+                              F nat from_ran tt) as X0;
     simpl in X0; rewrite <- X0; clear X0.
+
     assert (∀ f g, f ≈ g
               -> to (@adj _ _ _ _ ran_adjoint (Ran (Erase J) F) F) f
                ≈ to (@adj _ _ _ _ ran_adjoint (Ran (Erase J) F) F) g).
       intros; rewrite X0; reflexivity.
     simpl in X0; apply X0; clear X0.
+
     intros; simpl.
     apply (unique_property (ump_limits cone)).
-  - simpl.
-(*
+
+  - unfold adj_to; simpl in *; clear adj_to.
     evar (j : @ob J).
-
-    pose (iso_from_to (@adj _ _ _ _ ran_adjoint (Const (Lim F)) F) nat);
-    pose proof (iso_to_from
-                  ((@adj _ _ _ _ ran_adjoint
-                         (Ran (Erase J) F) F)) nat_identity tt);
-    pose proof (uniqueness (ump_limits cone));
-    pose proof (unique_property (ump_limits cone));
+    pose proof (unique_property (ump_limits cone) j) as X; simpl in X.
+    rewrite (@adj_unit _ _ _ _ ran_adjoint _ _ nat tt).
+    pose proof (@counit_fmap_unit _ _ Induced (Ran (Erase J))
+                                  ran_adjoint (Const (Lim F))) as X0;
+    simpl in X0; simpl.
+    rewrite comp_assoc.
+    rewrite <- (X0 j); clear X0.
+    comp_right.
+    unfold counit; simpl.
+    pose proof (@ump_cones _ _ _ cone); simpl in X0.
+    pose proof nat as X1.
+    pose proof (naturality[nat]) as X2.
+    pose proof (transform[nat]) as X3.
+    pose proof (uniqueness (ump_limits cone)) as X4.
+    pose proof (iso_from_to ((@adj _ _ _ _ ran_adjoint
+                                   (Const (Lim F)) F)) nat) as X5.
     simpl in *.
-    rewrite fmap_id in X.
-    rewrite <- (id_left (_ ())), <- X; clear X.
-    destruct (ump_limits cone); simpl in *.
-    pose proof (@ump_cones _ _ _ cone).
-    simpl in X.
-
-    given (nat : (Ran (Erase J) F) ○ Erase J ⟹ F). {
-      transform; simpl; intros.
-      + apply (@vertex_map _ _ _ cone).
-      + cat; apply (@ump_cones _ _ _ cone).
-      + cat; symmetry; apply (@ump_cones _ _ _ cone).
-    }
-
-    pose proof (iso_to_from
-                  ((@adj _ _ _ _ ran_adjoint
-                         (Const (Lim F)) F)) adj_to tt).
-    simpl in X.
-    unfold nat_identity in X; simpl in X.
-    unfold adj_to; simpl.
-
-    pose proof (@from_adj_nat_r _ _ _ _ ran_adjoint
-                              (Const (Lim F)) F
-                              F nat_identity adj_to).
-    simpl in X3.
-    assert (∀ f g, f ≈ g
-              -> to (@adj _ _ _ _ ran_adjoint (Ran (Erase J) F) F) f
-               ≈ to (@adj _ _ _ _ ran_adjoint (Ran (Erase J) F) F) g).
-      intros; rewrite X3; reflexivity.
-    simpl in X0; apply X0; clear X0.
-    intros; simpl.
-    apply (unique_property (ump_limits cone)).
-*)
+    destruct H0.
+    destruct ran_adjoint.
+    simpl in *.
 Admitted.
