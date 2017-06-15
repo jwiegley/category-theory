ebeeaa4dda099dc4fa0c4687c7a0a570627a46a4
John Wiegley <johnw@newartisans.com>
2017-06-15 03:02:15 -0700

    Remove all unneeded code; just one theorem left

--- src/Solver.v
+++ src/Solver.v
@@ -39,34 +39,9 @@ Next Obligation.
     contradiction.
 Defined.
 
-Lemma K_dec_on_type A (x : A) (eq_dec : ∀ y : A, x = y \/ x ≠ y)
-      (P : x = x -> Type) :
-  P (eq_refl x) -> ∀ p:x = x, P p.
-Proof.
-  intros.
-  elim (@Eqdep_dec.eq_proofs_unicity_on A _) with x (eq_refl x) p.
-    trivial.
-  exact eq_dec.
-Defined.
-
-Corollary Neq_dec' : ∀ x y : N, x = y \/ x ≠ y.
-Proof. intros; destruct (N.eq_dec x y); auto. Defined.
-
-Lemma Neq_dec_refl n : N.eq_dec n n = left (@eq_refl N n).
-Proof.
-  destruct (N.eq_dec n n).
-    refine (K_dec_on_type N n (Neq_dec' n)
-              (fun x => @left _ _ x = @left _ _ (@eq_refl N n)) _ _).
-    reflexivity.
-  contradiction.
-Defined.
-
 Unset Universe Polymorphism.
 
 Open Scope N_scope.
-
-Set Decidable Equality Schemes.
-Set Boolean Equality Schemes.
 
 Inductive Term : Set :=
   | Identity : N -> Term
@@ -87,133 +62,11 @@ Fixpoint TermCod (e : Term) : obj_idx :=
   | Compose f _ => TermCod f
   end.
 
-Inductive Subterm : Term -> Term -> Prop :=
-  | Compose1 : ∀ t1 t2, Subterm t1 (Compose t1 t2)
-  | Compose2 : ∀ t1 t2, Subterm t2 (Compose t1 t2).
+Section Denotation.
 
-Lemma Subterm_wf : well_founded Subterm.
-Proof.
-  constructor; intros.
-  inversion H; subst; simpl in *;
-  induction y;
-  induction t1 || induction t2;
-  simpl in *;
-  constructor; intros;
-  inversion H0; subst; clear H0;
-  try (apply IHy1; constructor);
-  try (apply IHy2; constructor).
-Defined.
-
-Section Symmetric_Product2.
-
-Variable A : Type.
-Variable leA : A -> A -> Prop.
-
-Inductive symprod2 : A * A -> A * A -> Prop :=
-  | left_sym2 :
-    ∀ x x':A, leA x x' -> ∀ y:A, symprod2 (x, y) (x', y)
-  | right_sym2 :
-    ∀ y y':A, leA y y' -> ∀ x:A, symprod2 (x, y) (x, y')
-  | both_sym2 :
-    ∀ (x x':A) (y y':A),
-      leA x x' ->
-      leA y y' ->
-      symprod2 (x, y) (x', y').
-
-Lemma Acc_symprod2 :
-  ∀ x:A, Acc leA x -> ∀ y:A, Acc leA y -> Acc symprod2 (x, y).
-Proof.
-  induction 1 as [x _ IHAcc]; intros y H2.
-  induction H2 as [x1 H3 IHAcc1].
-  apply Acc_intro; intros y H5.
-  inversion_clear H5; auto with sets.
-  apply IHAcc; auto.
-  apply Acc_intro; trivial.
-Defined.
-
-Lemma wf_symprod2 :
-  well_founded leA -> well_founded symprod2.
-Proof.
-  red.
-  destruct a.
-  apply Acc_symprod2; auto with sets.
-Defined.
-
-End Symmetric_Product2.
-
-Definition R := symprod2 Term Subterm.
-Arguments R /.
-
-Open Scope lazy_bool_scope.
-
-Set Transparent Obligations.
-
-Local Obligation Tactic := intros; try discriminate.
-
-Ltac equalities :=
-  repeat match goal with
-    | [ H : (_ &&& _) = true |- _ ] =>
-      rewrite <- andb_lazy_alt in H
-    | [ H : (_ && _) = true |- _ ] =>
-      apply andb_true_iff in H;
-      destruct H
-    | [ H : _ /\ _ |- _ ] =>
-      destruct H
-    | [ H : _ ∧ _ |- _ ] =>
-      destruct H
-    | [ H : (_ =? _) = true |- _ ] =>
-      apply N.eqb_eq in H
-    end;
-  simpl TermDom in *;
-  simpl TermCod in *;
-  subst.
-
-Ltac forward_reason :=
-  repeat match goal with
-  | |- context[match ?X with left _ => _ | right _ => None end = Some _] =>
-    destruct X; [| solve [ inversion 1 | inversion 2 ] ]
-  | |- context[match ?X with Some _ => _ | None => None end = Some _] =>
-    destruct X; [| solve [ inversion 1 | inversion 2 ] ]
-  | |- context[match ?X with left _ => _ | right _ => None end = Some _] =>
-    destruct X; [| solve [ inversion 1 | inversion 2 ] ]
-  end.
-
-Section denote.
-
-Variable (C : Category).
-Variable (objs : obj_idx -> C).
-Variable (arrs : arr_idx -> ∀ a b : obj_idx, option (objs a ~> objs b)).
-
-Fixpoint denote_infer dom (e : Term) :
-  option { cod : _ & objs dom ~> objs cod } :=
-  match e with
-  | Identity t =>
-    match N.eq_dec t dom with
-    | left pf_dom =>
-      Some (t; match pf_dom with
-               | eq_refl => @id C (objs t)
-               end)
-    | _ => None
-    end
-  | Morph dom' cod' n =>
-    match N.eq_dec dom' dom, arrs n dom' cod' with
-    | left pf_dom, Some arr =>
-      Some (cod'; match pf_dom with
-                  | eq_refl => arr
-                  end)
-    | _ , _ => None
-    end
-  | Compose g f =>
-    match denote_infer dom f with
-    | Some (t'; farr) =>
-      match denote_infer t' g with
-      | Some (t''; garr) =>
-        Some (t''; (garr ∘ farr))
-      | _ => None
-      end
-    | _ => None
-    end
-  end.
+Variable C : Category.
+Variable objs : obj_idx -> C.
+Variable arrs : arr_idx -> ∀ a b : obj_idx, option (objs a ~> objs b).
 
 Fixpoint denote dom cod (e : Term) :
   option (objs dom ~> objs cod) :=
@@ -240,20 +93,6 @@ Fixpoint denote dom cod (e : Term) :
     | _ , _ => None
     end
   end.
-
-Lemma denote_dom_cod : ∀ f dom cod f',
-  denote dom cod f = Some f' ->
-  TermDom f = dom /\ TermCod f = cod.
-Proof.
-  induction f; intros dom cod; simpl;
-  try solve [ forward_reason; auto ].
-  specialize (IHf2 dom (TermCod f2)).
-  specialize (IHf1 (TermCod f2) cod).
-  forward_reason.
-  destruct (IHf1 _ eq_refl).
-  destruct (IHf2 _ eq_refl).
-  tauto.
-Qed.
 
 Inductive Arrow : Set :=
   | Arr : N -> N -> N -> Arrow.
@@ -369,111 +208,12 @@ Fixpoint normalize_denote dom cod (xs : ArrowList) {struct xs} :
     in go cod f fs
   end.
 
-Goal ∀ x, normalize_denote x x (IdentityOnly x) = Some id.
-  intros; simpl.
-  rewrite Neq_dec_refl.
-  reflexivity.
-Qed.
-
-Goal ∀ x y f, normalize_denote x y (ArrowChain (Arr x y f) nil) = arrs f x y.
-  intros; simpl.
-  rewrite !Neq_dec_refl.
-  destruct (arrs f x y); auto.
-Qed.
-
-Goal ∀ x y z f g, normalize_denote x z (ArrowChain (Arr y z f) [Arr x y g]) =
-                  match arrs f y z, arrs g x y with
-                  | Some f, Some g => Some (f ∘ g)
-                  | _, _ => None
-                  end.
-  intros; simpl.
-  rewrite !Neq_dec_refl.
-  destruct (arrs f y z); auto.
-  destruct (arrs g x y); auto.
-Qed.
-
-Theorem normalize_chain_rule a xs dom cod :
-  normalize_denote dom cod (ArrowChain a xs) = None ->
-  match a with Arr x y f => y ≠ cod end \/
-  let fix go h ys :=
-      match h with
-      | Arr x y f =>
-        arrs f x y = None \/
-        match ys with
-        | nil => x ≠ dom
-        | cons (Arr z w g) xs => w ≠ x \/ go (Arr z w g) xs
-        end
-      end in
-  go a xs.
-Proof.
-  generalize dependent dom.
-  generalize dependent cod.
-  generalize dependent a.
-  induction xs; intros.
-    destruct a; simpl in *.
-    destruct (arrs n1 n n0).
-      destruct (N.eq_dec n dom);
-      destruct (N.eq_dec n0 cod);
-      subst; intuition.
-    intuition.
-  simpl.
-  destruct a, a0.
-  destruct (arrs n4 n2 n3).
-    destruct (N.eq_dec n3 cod); subst.
-    simpl in H.
-    simpl in IHxs.
-Admitted.
-
 Theorem normalize_compose : ∀ p1 p2 dom cod f,
   normalize_denote dom cod (normalize (Compose p1 p2)) = Some f ->
   ∃ g h, f ≈ g ∘ h ∧
-         normalize_denote (TermCod p2) cod (normalize p1) = Some g ∧
-         normalize_denote dom (TermCod p2) (normalize p2) = Some h.
+    normalize_denote (TermCod p2) cod (normalize p1) = Some g ∧
+    normalize_denote dom (TermCod p2) (normalize p2) = Some h.
 Proof.
-Admitted.
-
-Lemma normalize_dom_cod : ∀ p dom cod f,
-  normalize_denote dom cod (normalize p) = Some f ->
-  TermDom p = dom /\ TermCod p = cod.
-Proof.
-  induction p; intros.
-  - simpl in *.
-    destruct (N.eq_dec n cod); subst;
-    destruct (N.eq_dec cod dom); subst;
-    intuition; try discriminate;
-    destruct (N.eq_dec n dom); subst;
-    discriminate.
-  - simpl in *.
-    destruct (arrs n1 n n0); [|discriminate].
-    destruct (N.eq_dec n dom); subst;
-    destruct (N.eq_dec n0 cod); subst;
-    intuition; discriminate.
-  - specialize (IHp1 (TermCod p2) cod).
-    specialize (IHp2 dom (TermCod p2)).
-    apply normalize_compose in H.
-    destruct H, s, p, p.
-    destruct (IHp1 _ e0), (IHp2 _ e1);
-    clear IHp1 IHp2; subst.
-    intuition.
-Qed.
-
-Theorem normalize_denote_none_inv : ∀ p dom cod,
-  normalize_denote dom cod (normalize p) = None ->
-  match p with
-  | Identity x  => dom ≠ cod \/ x ≠ dom
-  | Morph x y f => dom ≠ x \/ cod ≠ y \/ arrs f x y = None
-  | Compose f g =>
-    normalize_denote (TermCod g) cod (normalize f) = None \/
-    normalize_denote dom (TermCod g) (normalize g) = None
-  end.
-Proof.
-  induction p; simpl; intros.
-  - destruct (N.eq_dec n dom);
-    destruct (N.eq_dec n cod); subst; auto; discriminate.
-  - destruct (arrs n1 n n0);
-    destruct (N.eq_dec n dom);
-    destruct (N.eq_dec n0 cod); subst; auto.
-  - admit.
 Admitted.
 
 Theorem normalize_sound : ∀ p dom cod f,
@@ -507,61 +247,28 @@ Proof.
     reflexivity.
 Qed.
 
-Lemma normalize_complete : ∀ f dom cod,
-  normalize_denote dom cod (normalize f) = None ->
-  denote dom cod f = None.
-Proof.
-  induction f; intros.
-  - simpl in *.
-    destruct (N.eq_dec n dom); subst;
-    destruct (N.eq_dec dom cod); subst; auto.
-  - simpl in *.
-    destruct (N.eq_dec n dom); subst;
-    destruct (N.eq_dec n0 cod); subst; auto.
-  - specialize (IHf1 (TermCod f2) cod).
-    specialize (IHf2 dom (TermCod f2)).
-    simpl.
-    apply normalize_denote_none_inv in H.
-    destruct H.
-      rewrite (IHf1 H).
-      destruct (denote dom (TermCod f2) f2); reflexivity.
-    rewrite (IHf2 H).
-    destruct (denote dom (TermCod f2) f2); reflexivity.
-Qed.
-
-Corollary normalize_to_denote : ∀ dom cod f g,
-  normalize f = normalize g ->
-    normalize_denote dom cod (normalize f) =
-    normalize_denote dom cod (normalize g).
-Proof. intros; rewrite H; reflexivity. Qed.
-
 Theorem normalize_apply dom cod : ∀ f g,
-  TermDom f = dom ->
-  TermCod f = cod ->
-  TermDom g = dom ->
-  TermCod g = cod ->
+  (∃ f', normalize_denote dom cod (normalize f) = Some f') ->
+  (∃ g', normalize_denote dom cod (normalize g) = Some g') ->
   normalize f = normalize g ->
   denote dom cod f ≈ denote dom cod g.
 Proof.
   intros.
-  apply (normalize_to_denote dom cod) in H3.
-  destruct (normalize_denote dom cod (normalize f)) eqn:Heqe.
-    destruct (normalize_sound _ dom cod _ Heqe), p.
-    destruct (normalize_denote dom cod (normalize g)) eqn:Heqe2.
-      destruct (normalize_sound _ dom cod _ Heqe2), p.
-      inversion H3; subst; clear H3.
-      rewrite e0, e2.
-      red.
-      rewrite <- e, <- e1.
-      reflexivity.
-    discriminate.
-  symmetry in H3.
-  rewrite (normalize_complete _ _ _ Heqe).
-  rewrite (normalize_complete _ _ _ H3).
+  destruct X.
+  destruct (normalize_sound _ dom cod _ e), p.
+  destruct X0.
+  destruct (normalize_sound _ dom cod _ e2), p.
+  inversion H; subst; clear H.
+  rewrite e1, e4.
+  red.
+  rewrite <- e0, <- e3.
+  rewrite H1 in e.
+  rewrite e in e2.
+  inversion e2.
   reflexivity.
 Qed.
 
-End denote.
+End Denotation.
 
 Import ListNotations.
 
@@ -687,10 +394,24 @@ Ltac categorical :=
         pose r2;
         change (denote _ objs arrs (TermDom r1) (TermCod r1) r1 ≈
                 denote _ objs arrs (TermDom r2) (TermCod r2) r2);
-        apply (normalize_apply _ objs arrs (TermDom r1) (TermCod r1)
-                 r1 r2 eq_refl eq_refl);
-        vm_compute;
-        auto
+
+        let r1' := eval simpl in (denote _ objs arrs (TermDom r1)
+                                         (TermCod r1) r1) in
+        pose r1';
+        match r1' with
+        | Some ?r1'' =>
+          let r2' := eval simpl in (denote _ objs arrs (TermDom r2)
+                                           (TermCod r2) r2) in
+          pose r2';
+          match r2' with
+          | Some ?r2'' =>
+            apply (normalize_apply _ objs arrs (TermDom r1) (TermCod r1) r1 r2);
+            vm_compute; auto;
+            eexists; reflexivity
+          | None => idtac
+          end
+        | None => idtac
+        end
       end
     end
   end.
