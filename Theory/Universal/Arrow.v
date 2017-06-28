90623fa1041d4fdfb9f383354aacfbb768df345a
John Wiegley <johnw@newartisans.com>
2017-06-28 01:17:42 -0700

    Improve the definition of universal arrows

--- Theory/Universal/Arrow.v
+++ Theory/Universal/Arrow.v
@@ -2,6 +2,7 @@ Set Warnings "-notation-overridden".
 
 Require Import Category.Lib.
 Require Export Category.Theory.Functor.
+Require Export Category.Theory.Unique.
 
 Generalizable All Variables.
 Set Primitive Projections.
@@ -13,20 +14,36 @@ Section UniversalArrow.
 Context `{C : Category}.
 Context `{D : Category}.
 
-(* A universal arrow. *)
-
 Require Import Category.Structure.Initial.
 Require Import Category.Construction.Comma.
 Require Import Category.Functor.Diagonal.
 
-Class Arrow (x : C) (U : D ⟶ C) := {
-  arrow_initial : @Initial (Const x ↓ U);
+(* A universal arrow is an initial object in the comma category (=(c) ↓ F). *)
+Class UniversalArrow (c : C) (F : D ⟶ C) := {
+  arrow_initial : @Initial (=(c) ↓ F);
+
   arrow_obj := snd (`1 (@initial_obj _ arrow_initial));
-
-  arrow : x ~> U arrow_obj;
-
-  ump_arrows {y : D} (h : x ~> U y) :
-    ∃ g : arrow_obj ~> y, fmap[U] g ∘ arrow ≈ h
+  arrow : c ~> F arrow_obj := `2 (@initial_obj _ arrow_initial)
 }.
+
+Notation "c ⟿ F" := (UniversalArrow c F) (at level 20) : category_theory_scope.
+
+(* The following UMP follows directly from the nature of initial objects in a
+   comma category. *)
+Corollary ump_universal_arrows `(c ⟿ F) `(h : c ~> F d) :
+  ∃! g : arrow_obj ~> d, h ≈ fmap[F] g ∘ arrow.
+Proof.
+  unfold arrow_obj, arrow; simpl.
+  destruct (@zero _ arrow_initial ((tt, d); h)), x.
+  simpl in *.
+  rewrite id_right in e.
+  exists h1.
+    assumption.
+  intros.
+  rewrite <- id_right in e.
+  rewrite <- id_right in X.
+  exact (snd (@zero_unique _ arrow_initial ((tt, d); h)
+                           ((tt, h1); e) ((tt, v); X))).
+Qed.
 
 End UniversalArrow.
