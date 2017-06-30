04c1ce882c5e999d671161ba371fd183ed84e2db
John Wiegley <johnw@newartisans.com>
2017-06-29 18:17:46 -0700

    More work in ArrowsOnly.v

--- Theory/Metacategory/ArrowsOnly.v
+++ Theory/Metacategory/ArrowsOnly.v
@@ -34,28 +34,23 @@ Record Metacategory := {
 
   (*a ∀ edges (X, Y) and (Y, Z), ∃ an edge (X, Z) which is equal to the
      composition of those edges. *)
-  composite_correct {k g f kg gf : arrow} :
-    composite k g kg ->
-    composite g f gf -> ∃ kgf, composite kg f kgf;
+  composite_correct {f g h fg gh : arrow} :
+    composite f g fg ->
+    composite g h gh -> ∃ fgh, composite fg h fgh;
 
-  composition_law {k g f kg gf : arrow} :
-    composite k g kg ->
-    composite g f gf ->
-    ∀ kgf, composite kg f kgf ↔ composite k gf kgf;
+  composition_law {f g h fg gh : arrow} :
+    composite f g fg ->
+    composite g h gh ->
+    ∀ fgh, composite fg h fgh ↔ composite f gh fgh;
 
   is_identity (u : arrow) :=
     (∀ f, defined f u -> composite f u f) ∧
     (∀ g, defined u g -> composite u g g);
 
-  identity_law (g : arrow) :
+  identity_law (f : arrow) :
     ∃ u u', is_identity u -> is_identity u' ->
-      composite g u g ∧ composite u' g g
+      composite f u f ∧ composite u' f f
 }.
-
-Corollary identity_law' (M : Metacategory) (g : arrow M) :
-  ∃ u u', is_identity M u -> is_identity M u' ->
-    composite M g u g ∧ composite M u' g g.
-Proof. intros; apply identity_law. Defined.
 
 Lemma MapsTo_dec (M : Metacategory) : ∀ elt k e (m : M.t elt),
   (∀ x y : elt, {x = y} + {x ≠ y}) ->
@@ -75,113 +70,189 @@ Proof.
   assumption.
 Qed.
 
-Lemma identity_dec (M : Metacategory) : ∀ u,
-  { identity M u } + { ~ identity M u }.
-Proof.
-Abort.
-
-(* Lemma arrows_ind (M : Metacategory) : *)
-(*   ∀ (P : arrow M → Prop), *)
-(*   (∀ (i : arrow M) (H : identity M i), P i) → *)
-(*   (∀ (i : arrow M) (H : ~ identity M i), P i) → *)
-(*   ∀ n : arrow M, P n. *)
-(* Proof. *)
-(* Abort. *)
-
 (* Every meta-category, defined wholly in terms of the axioms of category
    theory, gives rise to a category interpreted in the context of set
    theory. *)
 
 Local Obligation Tactic := intros.
 
-Lemma identity (M : Metacategory) :
-  ∀ i : arrow M, defined M i i ∧ is_identity M i -> composite M i i i.
-Proof.
-  intros.
-  apply H.
-  intuition.
-Defined.
+Section Metacategory.
 
-Lemma identity_composition_left (M : Metacategory) : ∀ f g fg u,
-  is_identity M u ->
-  composite M f g fg ->
-  defined M u f ->
-  defined M u fg.
+Context (M : Metacategory).
+
+Record object := {
+  obj_arr : arrow M;
+  obj_def : composite M obj_arr obj_arr obj_arr;
+  obj_id  : is_identity M obj_arr
+}.
+
+Record morphism (dom cod : object) := {
+  mor_arr : arrow M;
+  mor_dom : composite M mor_arr (obj_arr dom) mor_arr;
+  mor_cod : composite M (obj_arr cod) mor_arr mor_arr
+}.
+
+Arguments mor_arr {_ _} _.
+Arguments mor_dom {_ _} _.
+Arguments mor_cod {_ _} _.
+
+Definition identity (x : object) : morphism x x :=
+  {| mor_arr := obj_arr x
+   ; mor_dom := obj_def x
+   ; mor_cod := obj_def x |}.
+
+Lemma composition_left {x y z : object}
+      {f : morphism y z} {g : morphism x y} {fg : arrow M} :
+  composite M (mor_arr f) (mor_arr g) fg ->
+  composite M (obj_arr z) (mor_arr f) (mor_arr f) ->
+  composite M (obj_arr z) fg fg.
 Proof.
   intros.
-  destruct H.
-  specialize (c0 _ H1); clear H1.
-  destruct (composite_correct M c0 H0).
-  spose (fst (composition_law M c0 H0 _) m) as X.
-  eapply composite_defined.
+  destruct z, obj_id0, f, g; simpl in *.
+  specialize (c0 _ (composite_defined M H0)); clear H0.
+  destruct (composite_correct M c0 H).
+  spose (fst (composition_law M c0 H _) m) as X.
+  pose proof (FMapExt.F.MapsTo_fun H m); subst.
   apply X.
 Qed.
 
-Lemma identity_composition_right (M : Metacategory) : ∀ f g fg u,
-  is_identity M u ->
-  composite M f g fg ->
-  defined M g u ->
-  defined M fg u.
+Lemma composition_right {x y z : object}
+      {f : morphism y z} {g : morphism x y} {fg : arrow M} :
+  composite M (mor_arr f) (mor_arr g) fg ->
+  composite M (mor_arr g) (obj_arr x) (mor_arr g) ->
+  composite M fg (obj_arr x) fg.
 Proof.
   intros.
-  destruct H.
-  specialize (c _ H1); clear H1.
-  destruct (composite_correct M H0 c).
-  eapply composite_defined.
+  destruct x, obj_id0, f, g; simpl in *.
+  specialize (c _ (composite_defined M H0)); clear H0.
+  destruct (composite_correct M H c).
+  spose (fst (composition_law M H c _) m) as X.
+  pose proof (FMapExt.F.MapsTo_fun H X); subst.
   apply m.
 Qed.
 
-Program Definition Category_from_Metacategory (M : Metacategory) :
-  Category := {|
-  obj     := ∃ i : arrow M, defined M i i ∧ is_identity M i;
-  hom     := fun x y =>
-    ∃ f : arrow M, defined M f (`1 x) ∧ defined M (`1 y) f;
-  homset  := fun _ _ => {| Setoid.equiv := fun f g => `1 f = `1 g |};
+Definition composition {x y z : object}
+           (f : morphism y z) (g : morphism x y) : morphism x z :=
+  let fg := composite_correct M (mor_dom f) (mor_cod g) in
+  {| mor_arr := `1 fg
+   ; mor_dom   := composition_right (f:=f) (`2 fg) (mor_dom g)
+   ; mor_cod   := composition_left  (g:=g) (`2 fg) (mor_cod f) |}.
 
-  id      := fun x => (`1 x; (fst (`2 x), fst (`2 x)));
+Global Program Instance morphism_preorder : PreOrder morphism := {
+  PreOrder_Reflexive  := identity;
+  PreOrder_Transitive := fun _ _ _ g f => composition f g
+}.
 
-  compose := fun x y z f g =>
-    let fg := composite_correct
-                M (fst (snd (`2 y)) (`1 f) (fst (`2 f)))
-                  (snd (snd (`2 y)) (`1 g) (snd (`2 g))) in
-    (`1 fg;
-       (identity_composition_right M _ _ _ _ (snd (`2 x)) (`2 fg) (fst (`2 g)),
-        identity_composition_left  M _ _ _ _ (snd (`2 z)) (`2 fg) (snd (`2 f))))
+Global Program Instance morphism_setoid (x y : object) :
+  Setoid (morphism x y) := {
+  equiv := fun f g => mor_arr f = mor_arr g
+}.
+Next Obligation. equivalence; congruence. Qed.
+
+Lemma composition_identity_left {x y : object} (f : morphism x y) :
+  composition (identity y) f ≈ f.
+Proof.
+  destruct f; simpl.
+  destruct (composite_correct _ _ _); simpl in *.
+  eapply FMapExt.F.MapsTo_fun; eauto.
+Qed.
+
+Lemma composition_identity_right {x y : object} (f : morphism x y) :
+  composition f (identity x) ≈ f.
+Proof.
+  destruct f; simpl.
+  destruct (composite_correct _ _ _); simpl in *.
+  eapply FMapExt.F.MapsTo_fun; eauto.
+Qed.
+
+Lemma composition_associative {x y z w : object}
+      (f : morphism z w) (g : morphism y z) (h : morphism x y) :
+  composition f (composition g h) ≈ composition (composition f g) h.
+Proof.
+  destruct f, g, h; simpl.
+  repeat destruct (composite_correct _ _ _); simpl in *.
+  spose (fst (composition_law M m1 m x3) m2) as X1.
+  eapply FMapExt.F.MapsTo_fun; eauto.
+Qed.
+
+Lemma composition_respects {x y z : object} :
+  Proper (equiv ==> equiv ==> equiv) (@composition x y z).
+Proof.
+  proper.
+  destruct x0, y0, x1, y1; simpl in *; subst.
+  repeat destruct (composite_correct _ _ _); simpl in *.
+  eapply FMapExt.F.MapsTo_fun; eauto.
+Qed.
+
+Program Definition Category_from_Metacategory : Category := {|
+  obj     := object;
+  hom     := morphism;
+  homset  := fun _ _ => {| equiv := fun f g => mor_arr f = mor_arr g |};
+  id      := @identity;
+  compose := @composition;
+
+  compose_respects := @composition_respects;
+
+  id_left    := @composition_identity_left;
+  id_right   := @composition_identity_right;
+  comp_assoc := @composition_associative;
+  comp_assoc_sym := fun x y z w f g h =>
+    symmetry (@composition_associative x y z w f g h);
 |}.
-Next Obligation. equivalence; simpl in *; congruence. Qed.
-Next Obligation. proper; simpl in *; subst; auto. Admitted.
-Next Obligation.
-  simpl in *.
-  destruct f; simpl in *.
-  destruct (composite_correct M _ _); simpl.
-  eapply FMapExt.F.MapsTo_fun; eauto.
-Qed.
-Next Obligation.
-  destruct X0; simpl in *.
-  destruct (composite_correct M _ _ _ _ _ _ _); simpl.
-  eapply FMapExt.F.MapsTo_fun; eauto.
-Qed.
-Next Obligation.
-  destruct X1, X0; simpl in *.
-  repeat destruct (composite_correct M _ _ _ _ _ _ _); simpl.
-  spose (fst (composition_law M _ _ _ _ _ m1 m x3) m2) as X1.
-  eapply FMapExt.F.MapsTo_fun; eauto.
-Qed.
-Next Obligation.
-  symmetry.
-  destruct X1, X0; simpl in *.
-  repeat destruct (composite_correct M _ _ _ _ _ _ _); simpl.
-  spose (fst (composition_law M _ _ _ _ _ m1 m x3) m2) as X1.
-  eapply FMapExt.F.MapsTo_fun; eauto.
-Qed.
+
+End Metacategory.
+
+Arguments mor_arr _ {_ _} _.
+Arguments mor_dom _ {_ _} _.
+Arguments mor_cod _ {_ _} _.
 
 Notation "[map ]" := (M.empty _) (at level 9, only parsing).
-Notation "x +=> y" := (M.add x y) (at level 9, y at level 100, only parsing).
+Notation "x +=> y" := (M.add x (@of_nat_lt y _ _))
+  (at level 9, y at level 100, only parsing).
 Notation "[map  a ; .. ; b ]" := (a .. (b [map]) ..) (only parsing).
 
 Ltac cleanup :=
   try FMapExt.simplify_maps;
   repeat (right; intuition; try discriminate; FMapExt.simplify_maps).
+
+Local Obligation Tactic := program_simpl; try abstract omega.
+
+Lemma mapsto_inv : ∀ n f g (fg : Fin.t n) x y z m,
+  M.MapsTo (f, g) fg (M.add (x, y) z m) ->
+    (f = x ∧ g = y ∧ fg = z) ∨ M.MapsTo (f, g) fg m.
+Proof.
+Admitted.
+
+Ltac destruct_maps :=
+  repeat lazymatch goal with
+  | [ H : M.MapsTo _ _ (M.empty _) |- _ ] =>
+    contradiction (proj1 (FMapExt.F.empty_mapsto_iff _ _) H)
+
+  | [ H : M.MapsTo (?X, ?Y) ?F (M.add _ _ _) |- _ ] =>
+    apply mapsto_inv in H; destruct H as [[? [? ?]]|]
+
+  | [ H : ?X = ?Y |- context[M.MapsTo (` (to_nat ?X), _)] ] =>
+    rewrite H; cbn
+  | [ H : ?X = ?Y |- context[M.MapsTo (_, ` (to_nat ?X))] ] =>
+    rewrite H; cbn
+  | [ H : ?X = ?Y |- context[M.MapsTo (?X, _)] ] =>
+    rewrite H; cbn
+  | [ H : ?X = ?Y |- context[M.MapsTo (_, ?X)] ] =>
+    rewrite H; cbn
+  | [ H : ?X = ?Y |- context[M.MapsTo _ ?X] ] =>
+    rewrite H; cbn
+
+  | [ |- ∃ _, M.MapsTo (?X, ?Y) _ _ ] =>
+    match goal with
+      [ |- context[M.add (X, Y) ?F _ ] ] =>
+      exists F
+    end
+  | [ |- M.MapsTo (?X, ?Y) ?F (M.add (?X, ?Y) ?F _) ] =>
+    FMapExt.simplify_maps
+  | [ |- M.MapsTo _ _ (M.add _ _ _) ] =>
+    FMapExt.simplify_maps; right; split; [omega|]
+  end;
+  try congruence.
 
 Program Definition Two : Metacategory := {|
   num_arrows := 3;
@@ -190,188 +261,71 @@ Program Definition Two : Metacategory := {|
            ;    (1, 2) +=> 2
            ;    (2, 0) +=> 2 ]%nat
 |}.
-Next Obligation.
-  destruct f, g, h; simpl in *;
-  repeat (
-    FMapExt.simplify_maps; simplify;
-    try omega;
-    simpl in *;
-    try discriminate;
-    try omega).
-Qed.
-Next Obligation.
-  unfold arrow in *.
-  repeat (match goal with
-  | [ H : M.MapsTo _ _ (M.add _ _ _) |- _ ] =>
-    apply FMapExt.add_mapsto_iffT in H
-  | [ H : _ + _ |- _ ] => destruct H
-  | [ H : _ /\ _ |- _ ] => destruct H
-  | [ H : _ ∧ _ |- _ ] => destruct H
-  | [ H : ¬ (_ /\ _) |- _ ] =>
-    pose proof (proj1 (not_and_implication _ _) H eq_refl);
-    contradiction
-  | [ |- ∃ _, M.MapsTo (?X, ?Y) _ _ ] =>
-    match goal with
-      [ |- context[M.add (X, Y) ?F _ ] ] =>
-        exists F
-    end
-  end; subst; simpl in *);
-  try discriminate;
-  try congruence; cleanup.
-Qed.
+Next Obligation. destruct_maps. Qed.
 Next Obligation.
   split; intros;
-  repeat FMapExt.simplify_maps;
-  simpl in *;
-  destruct H0, H1, H2; subst;
-  intuition idtac;
-  try discriminate;
-  try omega; cleanup.
+  destruct_maps;
+  subst; discriminate.
+Qed.
+Next Obligation.
+  destruct f using caseS'.
+    eexists; eexists; intros.
+    simplify; simpl in *.
+    instantiate (1:=F1); simpl; destruct_maps.
+    instantiate (1:=F1); simpl; destruct_maps.
+  destruct f using caseS'.
+    eexists; eexists; intros.
+    simplify; simpl in *.
+    instantiate (1:=FS F1); simpl; destruct_maps.
+    instantiate (1:=FS F1); simpl; destruct_maps.
+  destruct f using caseS'.
+    eexists; eexists; intros.
+    simplify; simpl in *.
+    instantiate (1:=F1); simpl; destruct_maps.
+    instantiate (1:=FS F1); simpl; destruct_maps.
+  inversion f.
 Qed.
 
-(*
 Require Import Category.Instance.Two.
-
-Lemma composite_0_x_0 : forall x, composite Two 0%nat x 0%nat -> 0%nat = x.
-Proof.
-  intros.
-  unfold composite in H; simpl in H.
-  repeat match goal with
-    [ H : M.MapsTo _ _ (M.add _ _ _) |- _ ] =>
-    apply FMapExt.add_mapsto_iffT in H; destruct H; simplify; simpl in *; subst
-  end; auto.
-Defined.
-
-Lemma composite_x_0_0 : forall x, composite Two x 0%nat 0%nat -> 0%nat = x.
-Proof.
-  intros.
-  unfold composite in H; simpl in H.
-  repeat match goal with
-    [ H : M.MapsTo _ _ (M.add _ _ _) |- _ ] =>
-    apply FMapExt.add_mapsto_iffT in H; destruct H; simplify; simpl in *; subst
-  end; auto.
-  discriminate.
-Defined.
-
-Lemma composite_1_x_1 : forall x, composite Two 1%nat x 1%nat -> 1%nat = x.
-Proof.
-  intros.
-  unfold composite in H; simpl in H.
-  repeat match goal with
-    [ H : M.MapsTo _ _ (M.add _ _ _) |- _ ] =>
-    apply FMapExt.add_mapsto_iffT in H; destruct H; simplify; simpl in *; subst
-  end; auto.
-Defined.
-
-Lemma composite_x_1_1 : forall x, composite Two x 1%nat 1%nat -> 1%nat = x.
-Proof.
-  intros.
-  unfold composite in H; simpl in H.
-  repeat match goal with
-    [ H : M.MapsTo _ _ (M.add _ _ _) |- _ ] =>
-    apply FMapExt.add_mapsto_iffT in H; destruct H; simplify; simpl in *; subst
-  end; auto.
-Defined.
-
-Lemma composite_2_x_2 : forall x, composite Two 2%nat x 2%nat -> 0%nat = x.
-Proof.
-  intros.
-  unfold composite in H; simpl in H.
-  repeat match goal with
-    [ H : M.MapsTo _ _ (M.add _ _ _) |- _ ] =>
-    apply FMapExt.add_mapsto_iffT in H; destruct H; simplify; simpl in *; subst
-  end; auto.
-  discriminate.
-Defined.
-
-Lemma composite_x_2_2 : forall x, composite Two x 2%nat 2%nat -> 1%nat = x.
-Proof.
-  intros.
-  unfold composite in H; simpl in H.
-  repeat match goal with
-    [ H : M.MapsTo _ _ (M.add _ _ _) |- _ ] =>
-    apply FMapExt.add_mapsto_iffT in H; destruct H; simplify; simpl in *; subst
-  end; auto.
-  discriminate.
-  discriminate.
-Defined.
 
 Local Obligation Tactic := program_simpl; simpl in *.
 
-Lemma not_identity : ∀ x, (2 <= x)%nat -> identity Two x -> False.
+Program Definition Two_2_object (x : object Two) : TwoObj.
+Proof.
+  destruct x.
+  destruct obj_arr0 using caseS'.
+    exact TwoX.
+  destruct p using caseS'.
+    exact TwoY.
+  destruct p using caseS'.
+    unfold composite in obj_def0.
+    cbn in obj_def0.
+    apply mapsto_inv in obj_def0; destruct obj_def0 as [[? [? ?]]|].
+      discriminate.
+    apply mapsto_inv in m; destruct m as [[? [? ?]]|].
+      discriminate.
+    apply mapsto_inv in m; destruct m as [[? [? ?]]|].
+      discriminate.
+    apply mapsto_inv in m; destruct m as [[? [? ?]]|].
+      discriminate.
+    destruct_maps.
+  inversion p.
+Defined.
+
+Program Definition Two_2_morphism (x y : object Two) (f : morphism Two x y) :
+  TwoHom (Two_2_object x) (Two_2_object y).
 Proof.
 Admitted.
 
 Program Definition Two_to_Two : Category_from_Metacategory Two ⟶ _2 := {|
-  fobj := fun x => match `1 x with
-    | 0%nat => TwoX
-    | 1%nat => TwoY
-    | _     => False_rect _ (not_identity (`1 x) _ (`2 x))
-    end;
-  fmap := fun x y f => match `1 x, `1 y, `1 f with
-    | 0%nat, 0%nat, 0%nat => TwoIdX
-    | 1%nat, 1%nat, 1%nat => TwoIdY
-    | 0%nat, 1%nat, 2%nat => TwoXY
-    | _, _, _ => !
-    end
+  fobj := Two_2_object;
+  fmap := Two_2_morphism
 |}.
 Next Obligation.
-  destruct x. contradiction.
-  destruct x. contradiction.
-  omega.
-Qed.
-Next Obligation. subst; auto. Defined.
-Next Obligation. subst; auto. Defined.
-Next Obligation. subst; auto. Defined.
-Next Obligation. subst; auto. Defined.
-Next Obligation. subst; auto. Defined.
-Next Obligation. subst; auto. Defined.
-Next Obligation.
-  repeat match goal with
-  | [ H : Some _ = Some _ |- _ ] =>
-    inversion H; subst; clear H
-  | [ H : PO.eq _ _ |- _ ] =>
-    destruct H; simpl in *; subst
-  | [ H : ~ PO.eq _ _ |- _ ] =>
-    unfold PO.eq in H; simpl in H;
-    apply Decidable.not_and in H
-  | [ H : M.find _ (M.add _ _ _) = Some _ |- _ ] =>
-    rewrite FMapExt.F.add_o in H;
-    destruct (PO.eq_dec _ _)
-  | [ H : M.find _ (M.empty _) = Some _ |- _ ] =>
-    inversion H
-  | [ |- Decidable.decidable (?X = ?Y) ] =>
-    destruct (Nat.eq_dec X Y); [left|right]; assumption
-  end.
-  - omega.
-  - omega.
-  - eapply not_identity; eauto.
-  - eapply not_identity; eauto.
-Defined.
-Next Obligation.
+  proper.
+  destruct x0, y0; simpl in *; subst.
 Admitted.
 Next Obligation.
 Admitted.
 Next Obligation.
 Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-Next Obligation.
-Admitted.
-*)
