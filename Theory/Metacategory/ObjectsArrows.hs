6faa596cf7e239bc5e49dc7c5c0bddb0641a0318
John Wiegley <johnw@newartisans.com>
2017-07-04 13:06:06 -0700

    Simplify ObjectsArrows.hs even further

--- Theory/Metacategory/ObjectsArrows.hs
+++ Theory/Metacategory/ObjectsArrows.hs
@@ -1,43 +1,44 @@
+{- Given a count of objects in a category, such that for every object at index
+   n : ℕ there is an arrow to it from every other object { m : ℕ | m < n },
+   compute all the possible compositions of arrows in that category.
+
+   Note that this is sufficient to describe an "arrows only" definition of the
+   category, since the existence of "objects" is indicated by their identity
+   morphisms, and domain and codomain of morphisms by how they compose with
+   those identities. -}
+
 module Main where
 
+import Data.Bits
 import Data.List
 import System.Environment (getArgs)
 
--- Given a count of objects in a category, such that for every object at index
--- n : ℕ there is an arrow to it from every other object { m : ℕ | m < n },
--- compute all the possible compositions of arrows in that category.
---
--- Note that this is sufficient to describe an "arrows only" definition of the
--- category, since the existence of "objects" is indicated by their identity
--- morphisms, and domain and codomain of morphisms by how they compose with
--- those identities.
+-- 'step' folds over numbers, not including the starting number. So, for n =
+-- 3, 'f' is called with [2,1,0], in that order.
+step :: (Int -> a -> a) -> a -> Int -> a
+step f z = go
+  where
+    go 0 = z
+    go i = let j = i - 1 in f j (go j)
+{-# INLINE step #-}
 
 triangularNumber :: Int -> Int
-triangularNumber n = (n * (n + 1)) `div` 2
+triangularNumber n = shiftR (n * (n + 1)) 1
+{-# INLINE triangularNumber #-}
 
 composablePairsStep :: Int -> [((Int, Int), Int)]
-composablePairsStep n = go n
+composablePairsStep n = step go [] n
   where
     next = triangularNumber (n - 1)
 
-    go 0 = []
-    go j' = go' (n - j)
+    go i rest = step ((:) . k) rest (n - i)
       where
-        j = j' - 1
-        f = next + j
-
-        go' 0 = go j
-        go' i' = ((f + i, triangularNumber (i + j) + j), f) : go' i
-          where i = i' - 1
+        k j = let mor = next + i
+                  dom = triangularNumber (j + i) + i
+                  cod = mor + j in ((cod, dom), mor)
 
 composablePairs :: Int -> [((Int, Int), Int)]
-composablePairs 0 = []
-composablePairs j =
-    composablePairs (j - 1) ++ composablePairsStep j
-
-arrowCount :: Int -> Integer
-arrowCount 0 = 0
-arrowCount n = fromIntegral n^(2 :: Integer) + arrowCount (n - 1)
+composablePairs = step (\j rest -> composablePairsStep (j + 1) ++ rest) []
 
 -- The number of composable pairs for a category of N objects as described
 -- above is given by its tetrahedral number:
@@ -46,9 +47,8 @@ arrowCount n = fromIntegral n^(2 :: Integer) + arrowCount (n - 1)
 tetrahedralNumber :: Int -> Int
 tetrahedralNumber n = (n * (n + 1) * (n + 2)) `div` 6
 
-prop_composablePairs_length :: Int -> Bool
-prop_composablePairs_length n =
-    length (composablePairs n) == tetrahedralNumber n
+prop_composablePairs_len :: Int -> Bool
+prop_composablePairs_len n = length (composablePairs n) == tetrahedralNumber n
 
 coqSyntax :: String -> [((Int, Int), Int)] -> String
 coqSyntax name pairs = concat
@@ -68,8 +68,10 @@ main = do
         "length" -> case args of
             n:_ -> print $ length (composablePairs (read n :: Int))
             _ -> error "Bad arguments to length"
+
         "define" -> case args of
             name:n:_ ->
                 putStrLn $ coqSyntax name (composablePairs (read n :: Int))
             _ -> error "Bad arguments to define"
+
         _ -> error "Unknown command!"
