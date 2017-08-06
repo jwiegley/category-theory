6ef76a57689b52b6a3ef5efcce247d650211b9d8
John Wiegley <johnw@newartisans.com>
2017-08-06 13:16:23 -0700

    Further work in Decide.v

--- src/Decide.v
+++ src/Decide.v
@@ -107,124 +107,46 @@ Proof.
       cat.
 Qed.
 
-Lemma attempt {f x mid y} {f' : objs x ~> objs y} i i' j j' :
-  @termD C objs arrs x y f ≈ Some f' ->
-  @termD C objs arrs mid y i ≈ Some i' ->
-  @termD C objs arrs x mid j ≈ Some j' ->
-  arrows f = arrows i ++ arrows j -> f' ≈ i' ∘ j'.
+Lemma list_rect2 : ∀ (A : Type) (P : list A -> list A -> Type),
+  P [] [] ->
+  (∀ (a : A) (l1 : list A), P l1 [] -> P (a :: l1) []) ->
+  (∀ (b : A) (l2 : list A), P [] l2 -> P [] (b :: l2)) ->
+  (∀ (a b : A) (l1 l2 : list A), P l1 l2 -> P (a :: l1) (b :: l2))
+    -> ∀ l1 l2 : list A, P l1 l2.
 Proof.
-  unfold termD.
-  generalize dependent y.
-  generalize dependent mid.
-  generalize dependent x.
-  induction i; simpl; intros; repeat equalities; simpl_eq;
-  (destruct (termD_work objs _ _ j) eqn:?; [|contradiction]; destruct s);
-  (destruct (termD_work objs _ _ f) eqn:?; [|contradiction]; destruct s);
-  equalities.
-  - admit.
-  - destruct (arrs a); [|contradiction].
-    destruct s, s; equalities.
-    admit.
-  - unfold termD in *.
-    destruct (termD_work objs _ _ i2) eqn:?; [|contradiction]; destruct s.
-    destruct (termD_work objs _ _ i1) eqn:?; [|contradiction]; destruct s.
-    equalities.
-    rewrite <- X; clear X.
-    rewrite <- X0; clear X0.
-    rewrite <- X1; clear X1.
-    rewrite <- app_assoc in H.
-Admitted.
+  intros.
+  generalize dependent l2.
+  induction l1; simpl in *; intros;
+  induction l2; auto.
+Qed.
 
-(*
-Lemma arrows_compose {f x y} {f' : objs x ~> objs y} i j :
+Lemma arrows_compose {f x mid y} {f' : objs x ~> objs y} i i' j j' :
   arrows f = arrows i ++ arrows j ->
   @termD C objs arrs x y f ≈ Some f' ->
-  ∃ mid i' j',
-    @termD C objs arrs mid y i ≈ Some i' ∧
-    @termD C objs arrs x mid j ≈ Some j' ∧
-    f' ≈ i' ∘ j'.
+  @termD C objs arrs mid y i ≈ Some i' ->
+  @termD C objs arrs x mid j ≈ Some j' -> f' ≈ i' ∘ j'.
 Proof.
-  unfold termD.
-  generalize dependent j.
-  generalize dependent i.
-  generalize dependent y.
-  generalize dependent x.
-  generalize dependent f.
-  induction i; simpl; intros; subst; auto.
-  - destruct j; simpl in *; admit.
-  - destruct j; simpl in *; admit.
-  - 
-  induction f; simpl; intros; subst; auto.
-  - admit.
-  - admit.
-  - destruct (termD_work objs _ _ f2) eqn:?; [|contradiction]; destruct s.
-    destruct (termD_work objs _ _ f1) eqn:?; [|contradiction]; destruct s.
-    equalities; simpl_eq.
+  (* Unpack as much "static" information as we can from the hypothesis. *)
+  unfold termD; intros.
+  destruct (termD_work objs _ _ f) eqn:?; [|contradiction]; destruct s.
+  destruct (termD_work objs _ _ i) eqn:?; [|contradiction]; destruct s.
+  destruct (termD_work objs _ _ j) eqn:?; [|contradiction]; destruct s.
+  destruct (Eq_eq_dec x0 y); [|contradiction]; subst.
+  destruct (Eq_eq_dec x1 y); [|contradiction]; subst.
+  destruct (Eq_eq_dec x2 mid); [|contradiction]; subst.
+  simpl_eq.
+  red in X.
+  red in X0.
+  red in X1.
+  rewrite <- X, <- X0, <- X1.
+  clear X X0 X1 f' i' j'.
+  rename h into f'.
+  rename h0 into i'.
+  rename h1 into j'.
 
-    specialize (IHf1 _ _ h0 f1 Identity).
-    rewrite Heqo0, Eq_eq_dec_refl in IHf1.
-    rewrite app_nil_r in IHf1.
-    specialize (IHf1 eq_refl (reflexivity _)).
-
-    specialize (IHf2 _ _ h Identity f2).
-    rewrite Heqo, Eq_eq_dec_refl in IHf2.
-    specialize (IHf2 eq_refl (reflexivity _)).
-
-    destruct IHf1, s, s, p, p.
-    destruct IHf2, s, s, p, p.
-    simpl in *.
-    equalities.
-
-    rewrite Heqo0 in e.
-    rewrite Pos_eq_dec_refl in e.
-
-    exists x1, h0, h.
-
-    split.
-    + 
-    + split.
-      * admit.
-      * now symmetry.
-
-    destruct (termD_work objs _ _ i) eqn:?;
-    destruct (termD_work objs _ _ j) eqn:?;
-    try destruct s.
-    + repeat equalities.
-      * admit.
-      * admit.
-      * 
-        destruct (termD_work objs _ _ g) eqn:?; [|discriminate]; destruct s.
-    
+  (* Now we must induction, effectively over the two terms, to determine if
+     they result in equivalent morphisms. *)
 Admitted.
-*)
-(*
-    destruct (termD_work objs _ _ i) eqn:?; [|contradiction]; destruct s.
-    destruct (termD_work objs _ _ j) eqn:?; [|contradiction]; destruct s.
-    repeat equalities; simpl_eq.
-      admit.
-    admit.
-  - assert ((arrows i = [a] ∧ arrows j = []) ∨
-            (arrows j = [a] ∧ arrows i = [])). {
-      destruct (arrows i); simpl in H.
-        now right.
-      destruct (arrows j); simpl in H.
-        rewrite app_nil_r in H.
-        now left.
-      destruct l; simpl in H.
-        inversion H.
-      destruct l0; simpl in H; inversion H.
-    }
-    destruct H0, p.
-    + pose proof (arrows_morph (x:=x) (y:=mid) (f':=j') e).
-      unfold termD in X1.
-      destruct (arrs a).
-        destruct s, s.
-        admit.
-      admit.
-    + admit.
- - specialize (IHf1 _ _ i' _ f' j').
-Admitted.
-*)
 
 Lemma arrows_decide {x y f f' g g'} :
   @termD C objs arrs x y f = Some f' ->
@@ -273,15 +195,13 @@ Proof.
     rewrite <- (Eqdep_dec.inj_pair2_eq_dec _ Eq_eq_dec _ _ _ _ H2).
     clear H2.
     inversion Heqo0; subst; clear Heqo0.
-    pose proof (attempt (x:=x) (mid:=x0) (y:=y) (f:=g) (f':=g') f1 h0 f2 h).
+    pose proof (arrows_compose (x:=x) (mid:=x0) (y:=y) (f:=g) (f':=g') f1 h0 f2 h).
     unfold termD in X.
     rewrite Heqo1, Heqo2, Heqo3 in X.
     rewrite !Eq_eq_dec_refl in X.
     rewrite <- (Eqdep_dec.inj_pair2_eq_dec _ Eq_eq_dec _ _ _ _ H2) in X |- *.
-    clear H2.
-    specialize (X (reflexivity _) (reflexivity _) (reflexivity _)).
-    equalities; simpl_eq.
-    intuition.
+    now specialize (X (symmetry H1)
+                      (reflexivity _) (reflexivity _) (reflexivity _)).
 Qed.
 
 End Decide.
