374ff75cfb669d6b944d165b3b2a2ba2abb74d31
John Wiegley <johnw@newartisans.com>
2017-08-06 13:15:51 -0700

    Delete dead code

--- src/Normal.v
+++ src/Normal.v
@@ -19,65 +19,6 @@ Variable arrs : ∀ f : arr_idx, option (∃ x y, objs x ~{C}~> objs y).
 
 Import EqNotations.
 
-(*
-Inductive Arrow : Term -> Set :=
-
-  | Here  : ∀ a, Arrow (Morph a)
-  | Left  : ∀ f g, Arrow f -> Arrow (Compose f g)
-  | Right : ∀ f g, Arrow g -> Arrow (Compose f g).
-
-Definition Arrow_inv_t : forall t, Arrow t -> Type.
-Proof.
-  intros [] H.
-  - exact False.
-  - exact (H = Here a).
-  - exact ((∃ H', H = Left f g H') + (∃ H', H = Right f g H')).
-Defined.
-
-Lemma Arrow_inv t f : Arrow_inv_t t f.
-Proof.
-  destruct f; simpl; equalities; simpl_eq; auto.
-  - left. exists f0; auto.
-  - right. exists f0; auto.
-Defined.
-
-Lemma Arrow_Morph `(H : Arrow (Morph a)) : H = Here a.
-Proof. exact (Arrow_inv _ H). Defined.
-
-Lemma Arrow_Compose `(H : Arrow (Compose f g)) :
-  (∃ H', H = Left f g H') + (∃ H', H = Right f g H').
-Proof. exact (Arrow_inv _ H). Defined.
-
-Fixpoint get_arrow `(f : Arrow t) : arr_idx :=
-  match f with
-  | Here a      => a
-  | Left _ _ x  => get_arrow x
-  | Right _ _ x => get_arrow x
-  end.
-
-Function arrow_bequiv {t t' : Term} (p : Arrow t) (q : Arrow t') : bool :=
-  match get_arrow p, get_arrow q with
-  | (o, f), (o', f') => Eq_eqb o o' && Eq_eqb f f'
-  end.
-
-Function arrows (t : Term) : list (Arrow t) :=
-  match t with
-  | Identity _    => nil
-  | Morph x y a   => [Here x y a]
-  | Compose m f g => map (Left m f g) (arrows f) ++
-                     map (Right m f g) (arrows g)
-  end.
-
-Function arrows_bequiv {t t' : Term}
-         (ps : list (Arrow t)) (qs : list (Arrow t')) : bool :=
-  match ps, qs with
-  | nil, nil => true
-  | cons p ps, cons q qs =>
-    arrow_bequiv p q &&& arrows_bequiv ps qs
-  | _, _ => false
-  end.
-*)
-
 Function arrows (t : Term) : list arr_idx :=
   match t with
   | Identity    => nil
