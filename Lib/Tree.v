4c192fef4f4f3e501cd21c350893040016e36a21
John Wiegley <johnw@newartisans.com>
2018-03-20 00:09:54 -0700

    Work in Tree.v

--- Lib/Tree.v
+++ Lib/Tree.v
@@ -6,7 +6,6 @@ Unset Equations WithK.
 
 Require Import Category.Lib.
 Require Import Category.Lib.TList.
-(* Require Import Category.Lib.IList. *)
 
 Generalizable All Variables.
 
@@ -15,131 +14,102 @@ Section Tree.
 Variables A : Type.
 
 Inductive tree : Type :=
-  | Leaf : tree
-  | Node (f : A) : tree
+  | Leaf (f : A) : tree
   | Branch (f g : tree) : tree.
 
-Fixpoint inorder (t : tree) : list A :=
+Fixpoint prefold {C : Type} (f : A -> C -> C) (z : C) (t : tree) : C :=
   match t with
-  | Leaf => nil
-  | Node f => f :: nil
-  | Branch f g => inorder f ++ inorder g
+  | Leaf x => f x z
+  | Branch l r => prefold f (prefold f z r) l
   end.
+
+Fixpoint preorder_direct (t : tree) : list A :=
+  match t with
+  | Leaf f => f :: nil
+  | Branch f g => preorder_direct f ++ preorder_direct g
+  end.
+
+Definition preorder : tree -> list A := prefold cons nil.
+
+Lemma prefold_app xs ys t :
+  prefold cons (xs ++ ys)%list t = (prefold cons xs t ++ ys)%list.
+Proof.
+  generalize dependent ys.
+  generalize dependent xs.
+  induction t; simpl; intros; auto.
+  now rewrite IHt2, IHt1.
+Qed.
+
+Lemma preorder_spec t :
+  preorder t = preorder_direct t.
+Proof.
+  unfold preorder.
+  induction t; simpl; auto.
+  rewrite IHt2.
+  replace (preorder_direct t2)
+    with ([] ++ preorder_direct t2)%list at 1 by auto.
+  now rewrite prefold_app, IHt1.
+Qed.
 
 End Tree.
 
 Section TTree.
 
-Record TreeDesc := {
-  indexType : Type;
-  valueType : Type;
-  valueDom : valueType -> indexType;
-  valueCod : valueType -> indexType;
-}.
+Variables A B : Type.
+Variables dom cod : B -> A.
 
-Context {td : TreeDesc}.
+Inductive ttree : A -> A -> Type :=
+  | TLeaf (f : B) : ttree (dom f) (cod f)
+  | TBranch (d m c : A) (f : ttree d m) (g : ttree m c) : ttree d c.
 
-Inductive ttree : indexType td -> indexType td -> Type :=
-  | TLeaf (d : indexType td) : ttree d d
-  | TNode (f : valueType td) : ttree (valueDom td f) (valueCod td f)
-  | TBranch (d m c : indexType td) (f : ttree d m) (g : ttree m c) : ttree d c.
+Arguments TBranch {d m c} f g.
 
-Fixpoint tinorder `(t : ttree d c) : tlist (fun _ _ => valueType td) d c :=
+Equations tprefold {C : A -> A -> Type} {d c}
+          (f : forall i (x : B), C (cod x) i -> C (dom x) i)
+          {i} (z : C c i) (t : ttree d c) : C d i :=
+  tprefold f z (TLeaf x) := f _ x z;
+  tprefold f z (TBranch l r) := tprefold f (tprefold f z r) l.
+
+Definition tpreorder {d c} : ttree d c -> tlist (fun _ _ => B) d c :=
+   tprefold (C:=tlist (fun _ _ => B)) (fun _ x l => x ::: l) tnil.
+
+Fixpoint tpreorder_direct {d c} (t : ttree d c) : tlist (fun _ _ => B) d c :=
   match t with
-  | TLeaf _ => tnil
-  | TNode f => f ::: tnil
-  | TBranch d m c f g => tinorder f +++ tinorder g
+  | TLeaf f => f ::: tnil
+  | TBranch f g => tpreorder_direct f +++ tpreorder_direct g
   end.
 
-Record Related := {
-  forgetIndex : Type;
-  forgetType : Type;
-  project : valueType td -> forgetType;
-  recover : forgetType -> option (valueType td);
-  project_recover (x : valueType td) : recover (project x) = Some x;
-  recover_project (x : forgetType) y : recover x = Some y -> project y = x
-}.
+Lemma tprefold_app {d c i j} (t : ttree d c)
+      (xs : tlist (fun _ _ => B) c i) (ys : tlist (fun _ _ => B) i j) :
+  tprefold (C:=tlist (fun _ _ => B)) (fun _ x l => x ::: l) (xs +++ ys) t
+    = tprefold (C:=tlist (fun _ _ => B)) (fun _ x l => x ::: l) xs t +++ ys.
+Proof.
+  generalize dependent ys.
+  generalize dependent xs.
+  generalize dependent j.
+  generalize dependent i.
+  generalize dependent c.
+  generalize dependent d.
+  induction t; simpl; intros; auto.
+    rewrite !tprefold_equation_1.
+    now rewrite tlist_app_comm_cons.
+  rewrite !tprefold_equation_2.
+  now rewrite IHt2, IHt1.
+Qed.
 
-Context {rd : Related}.
-Context `{EqDec (indexType td)}.
-
-Context {E : Type}.
-
-Variable tleaf : E.
-Variable tnode : forall f : forgetType rd, E.
-Variable tbranch : E -> E -> E.
-
-Fixpoint tfold `(t : ttree d c) : E :=
-  match t with
-  | TLeaf _ => tleaf
-  | TNode f => tnode (project rd f)
-  | TBranch d m c f g => tbranch (tfold f) (tfold g)
-  end.
-
-Import EqNotations.
-
-Context {D : indexType td -> indexType td -> Type}.
-
-Variable leaf : forall d, D d d.
-Variable node : forall f : valueType td, D (valueDom td f) (valueCod td f).
-Variable branch : forall d m c, D d m -> D m c -> D d c.
-
-Fixpoint fold_work d (e : tree (forgetType rd)) : option (∃ c, D d c) :=
-  match e with
-  | Leaf _ => Some (d; leaf d)
-  | Node _ a =>
-    match recover rd a with
-    | Some f =>
-      match eq_dec (valueDom td f) d with
-      | left H =>
-        Some (valueCod td f; rew [fun x => D x _] H in node f)
-      | _ => None
-      end
-    | _ => None
-    end
-  | Branch _ f g =>
-    match fold_work d f with
-    | Some (m; f) =>
-      match fold_work m g with
-      | Some (c; g) => Some (c; branch d m c f g)
-      | _ => None
-      end
-    | _ => None
-    end
-  end.
-
-Definition fold d c (e : tree (forgetType rd)) : option (D d c) :=
-  match fold_work d e with
-  | Some (y; e) =>
-    match eq_dec y c with
-    | Specif.left H => Some (rew [fun y => D _ y] H in e)
-    | _ => None
-    end
-  | _ => None
-  end.
+Lemma tpreorder_spec d c (t : ttree d c) :
+  tpreorder t = tpreorder_direct t.
+Proof.
+  unfold tpreorder.
+  generalize dependent c.
+  generalize dependent d.
+  induction t; simpl; auto.
+  rewrite tprefold_equation_2.
+  rewrite IHt2.
+  replace (tpreorder_direct t2)
+    with (tnil +++ tpreorder_direct t2)%list at 1 by auto.
+  rewrite tprefold_app.
+  now rewrite IHt1.
+Qed.
 
 End TTree.
-
-Section Functions.
-
-Context {td : TreeDesc}.
-Context `{EqDec (indexType td)}.
-Context {rd : @Related td}.
-
-Fixpoint demote `(t : ttree d c) : tree (forgetType rd) :=
-  tfold (Leaf _) (Node _) (Branch _) t.
-
-Definition promote (d c : indexType td) (e : tree (forgetType rd)) :
-  option (ttree d c) := fold TLeaf TNode TBranch d c e.
-
-(*
-Inductive trose : indexType td -> indexType td -> Type :=
-  | RLeaf (d : indexType td) : trose d d
-  | RNode (f : valueType td) (arity : nat)
-          (tys : Vector.t (indexType td * indexType td) arity)
-          (xs : list (trose (valueDom td f) (valueCod td f)))  :
-      trose (valueDom td f) (valueCod td f)
-  | RBranch (d m c : indexType td) (f : trose d m) (g : trose m c) : trose d c.
-*)
-
-End Functions.
