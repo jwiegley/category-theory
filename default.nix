4cd0009b0528b225ae03a17a96461963865d22ea
John Wiegley <johnw@newartisans.com>
2018-05-07 18:17:24 -0700

    Add equations_8_8

--- default.nix
+++ default.nix
@@ -11,15 +11,38 @@
 
 let inherit (nixpkgs) pkgs;
 
-  # QuickChick_8_8 = cpkgs:
-  #   self.callPackage ./coq/QuickChick.nix { inherit (cpkgs) coq; };
-
-  # equations_8_8 = cpkgs:
-  #   self.callPackage ./coq/equations.nix { inherit (cpkgs) coq; };
-
   coqPackages = let cpkgs = pkgs.mkCoqPackages pkgs.${compiler}; in cpkgs // {
-    # QuickChick = QuickChick_8_8 cpkgs;
-    # equations = equations_8_8 cpkgs;
+    equations =
+      if compiler == "coq_8_8"
+      then with pkgs; with cpkgs; stdenv.mkDerivation rec {
+        name = "coq${coq.coq-version}-equations-${version}";
+        version = "1.0";
+
+        src = fetchFromGitHub {
+          owner = "mattam82";
+          repo = "Coq-Equations";
+          rev = "v1.0-8.8";
+          sha256 = "0dd7zd5j2sv5cw3mfwg33ss2vcj634q3qykakc41sv7f3rfgqfnn";
+        };
+
+        buildInputs = [ coq.ocaml coq.camlp5 coq.findlib coq ];
+
+        preBuild = "coq_makefile -f _CoqProject -o Makefile";
+
+        installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
+
+        meta = with stdenv.lib; {
+          homepage = https://mattam82.github.io/Coq-Equations/;
+          description = "A plugin for Coq to add dependent pattern-matching";
+          maintainers = with maintainers; [ jwiegley ];
+          platforms = coq.meta.platforms;
+        };
+
+        passthru = {
+          compatibleCoqVersions = v: builtins.elem v [ "8.8" ];
+        };
+      }
+      else cpkgs.equations;
   };
 
 in
