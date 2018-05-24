f5085d4fab33ddebc64af949412e3c4d434c8d92
John Wiegley <johnw@newartisans.com>
2018-05-23 19:01:07 -0500

    Simplify default.nix based on my own environment

--- default.nix
+++ default.nix
@@ -12,44 +12,22 @@
 with pkgs.${packages}; pkgs.stdenv.mkDerivation rec {
   name = "category-theory";
   version = "1.0";
+
   src =
     if pkgs.lib.inNixShell
     then null
     else
-      builtins.filterSource (path: type:
-        let baseName = baseNameOf path; in
-        !( type == "directory"
-           && builtins.elem baseName [".git"])
-        &&
-        !( type == "unknown"
-           || baseName == ".coq-version"
-           || baseName == "CoqMakefile.conf"
-           || baseName == "Makefile.coq"
-           || baseName == "Makefile.coq-old.conf"
-           || baseName == "result"
-           || pkgs.stdenv.lib.hasSuffix ".a" path
-           || pkgs.stdenv.lib.hasSuffix ".o" path
-           || pkgs.stdenv.lib.hasSuffix ".cmi" path
-           || pkgs.stdenv.lib.hasSuffix ".cmo" path
-           || pkgs.stdenv.lib.hasSuffix ".cmx" path
-           || pkgs.stdenv.lib.hasSuffix ".cmxa" path
-           || pkgs.stdenv.lib.hasSuffix ".cmxs" path
-           || pkgs.stdenv.lib.hasSuffix ".ml" path
-           || pkgs.stdenv.lib.hasSuffix ".mli" path
-           || pkgs.stdenv.lib.hasSuffix ".ml.d" path
-           || pkgs.stdenv.lib.hasSuffix ".ml4" path
-           || pkgs.stdenv.lib.hasSuffix ".ml4.d" path
-           || pkgs.stdenv.lib.hasSuffix ".mllib.d" path
-           || pkgs.stdenv.lib.hasSuffix ".aux" path
-           || pkgs.stdenv.lib.hasSuffix ".glob" path
-           || pkgs.stdenv.lib.hasSuffix ".v.d" path
-           || pkgs.stdenv.lib.hasSuffix ".vo" path))
-      ./.;
+    if pkgs ? coqFilterSource
+    then pkgs.coqFilterSource [] ./.
+    else ./.;
+
   buildInputs = [ coq coq.ocaml coq.camlp5 coq.findlib equations ];
   enableParallelBuilding = true;
+
   buildPhase = "make -j$NIX_BUILD_CORES";
   preBuild = "coq_makefile -f _CoqProject -o Makefile";
   installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
+
   env = pkgs.buildEnv { name = name; paths = buildInputs; };
   passthru = {
     compatibleCoqVersions = v: builtins.elem v [ "8.6" "8.7" "8.8" ];
