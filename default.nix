9a118d4528c67af46987e9d65aad7080a789c38c
John Wiegley <johnw@newartisans.com>
2018-08-01 09:46:47 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { packages ? "coqPackages_8_7"
-, rev      ? "49bdae006e66e70ad3245a463edc01b5749250d3"
-, sha256   ? "1ijsifmap47nfzg0spny94lmj66y3x3x8i6vs471bnjamka3dx8p"
+, rev      ? "d1ae60cbad7a49874310de91cd17708b042400c8"
+, sha256   ? "0a1w4702jlycg2ab87m7n8frjjngf0cis40lyxm3vdwn7p4fxikz"
 , pkgs     ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256; }) {
