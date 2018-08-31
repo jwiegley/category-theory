6eda813ec973c1f865c1e583f398d021e133f7c6
John Wiegley <johnw@newartisans.com>
2018-08-31 12:58:52 -0700

    Simplify the proofs of Product_Monoid

--- Structure/Monoid.v
+++ Structure/Monoid.v
@@ -4,6 +4,7 @@ Require Import Category.Lib.
 Require Export Category.Functor.Bifunctor.
 Require Export Category.Structure.Monoidal.
 Require Export Category.Structure.Monoidal.Internal.Product.
+Require Import Category.Structure.Closed.
 
 Generalizable All Variables.
 Set Primitive Projections.
@@ -57,137 +58,92 @@ Context `{@Terminal C}.
     in the usual sense (i.e., mappend reducing two objects to one). *)
 Definition Monoid := @MonoidObject C InternalProduct_Monoidal.
 
-Program Instance Product_Monoid `(X : Monoid x) `(Y : Monoid y) :
+Global Program Instance Product_Monoid `(X : Monoid x) `(Y : Monoid y) :
   @MonoidObject C InternalProduct_Monoidal (x × y) := {|
-  mempty  := mempty (MonoidObject:=X) △ mempty (MonoidObject:=Y);
+  mempty  := @mempty _ _ _ X △ @mempty _ _ _ Y;
   mappend := split (@mappend _ _ _ X) (@mappend _ _ _ Y) ∘ toggle
 |}.
 Next Obligation.
   spose (@mempty_left _ _ _ X) as HX.
   spose (@mempty_left _ _ _ Y) as HY.
-  rewrite <- (split_fork mempty[X]) in HX.
-  rewrite <- (split_fork mempty[Y]) in HY.
-  rewrite fork_exl_exr, id_right in HX.
-  rewrite fork_exl_exr, id_right in HY.
-  simpl.
+  assert ((mempty[X] △ mempty[Y] ∘ exl) △ (id[x × y] ∘ exr)
+            ≈ split (mempty[X] △ mempty[Y]) id[x × y])
+    by (unfork; cat).
+  rewrite X0; clear X0.
   unfold toggle.
+  rewrite <- !comp_assoc.
   rewrite <- fork_comp.
+  rewrite !split_comp.
+  rewrite exl_fork, exr_fork.
+  rewrite !id_right.
+  rewrite <- (id_right mempty[X]).
+  rewrite <- (id_left exl).
+  rewrite <- (id_right mempty[Y]).
+  rewrite <- (id_left exr).
+  rewrite <- !split_comp.
   rewrite split_fork.
-  rewrite <- fork_comp.
-  unfork.
-  rewrite <- (split_fork mempty[X]).
-  rewrite fork_exl_exr, id_right.
-  rewrite <- (split_fork mempty[Y]).
-  rewrite fork_exl_exr, id_right.
-  unfold split in *.
-  rewrite second_id, id_right in HX.
-  rewrite second_id, id_right in HY.
   rewrite !comp_assoc.
   rewrite HX, HY.
-  unfold second.
-  rewrite !exr_fork.
-  rewrite fork_comp.
-  rewrite fork_exl_exr, id_left.
-  reflexivity.
+  unfold split; cat.
+  now rewrite fork_comp; cat.
 Qed.
 Next Obligation.
   spose (@mempty_right _ _ _ X) as HX.
   spose (@mempty_right _ _ _ Y) as HY.
-  rewrite <- (split_fork _ mempty[X]) in HX.
-  rewrite <- (split_fork _ mempty[Y]) in HY.
-  rewrite fork_exl_exr, id_right in HX.
-  rewrite fork_exl_exr, id_right in HY.
-  simpl.
+  assert ((id[x × y] ∘ exl) △ (mempty[X] △ mempty[Y] ∘ exr)
+            ≈ split id[x × y] (mempty[X] △ mempty[Y]))
+    by (unfork; cat).
+  rewrite X0; clear X0.
   unfold toggle.
+  rewrite <- !comp_assoc.
   rewrite <- fork_comp.
+  rewrite !split_comp.
+  rewrite exl_fork, exr_fork.
+  rewrite !id_right.
+  rewrite <- (id_right mempty[X]).
+  rewrite <- (id_left exl).
+  rewrite <- (id_right mempty[Y]).
+  rewrite <- (id_left exr).
+  rewrite <- !split_comp.
   rewrite split_fork.
-  rewrite <- fork_comp.
-  unfork.
-  rewrite <- (split_fork _ mempty[X]).
-  rewrite fork_exl_exr, id_right.
-  rewrite <- (split_fork _ mempty[Y]).
-  rewrite fork_exl_exr, id_right.
-  unfold split in *.
-  rewrite first_id, id_left in HX.
-  rewrite first_id, id_left in HY.
-  rewrite !first_second.
   rewrite !comp_assoc.
   rewrite HX, HY.
-  unfold first.
-  rewrite !exl_fork.
-  rewrite fork_comp.
-  rewrite fork_exl_exr, id_left.
-  reflexivity.
+  unfold split; cat.
+  now rewrite fork_comp; cat.
 Qed.
 Next Obligation.
   spose (@mappend_assoc _ _ _ X) as HX.
   spose (@mappend_assoc _ _ _ Y) as HY.
-  rewrite <- (split_fork mappend[X]) in HX.
-  rewrite <- (split_fork mappend[Y]) in HY.
-  rewrite fork_exl_exr, id_right in HX.
-  rewrite fork_exl_exr, id_right in HY.
-  rewrite <- (split_fork _ mappend[X]) in HX.
-  rewrite <- (split_fork _ mappend[Y]) in HY.
-  rewrite fork_exl_exr, id_right in HX.
-  rewrite fork_exl_exr, id_right in HY.
-  rewrite <- !comp_assoc in HX.
-  rewrite split_fork, id_left in HX.
-  rewrite <- !comp_assoc in HY.
-  rewrite split_fork, id_left in HY.
-  unfold split at 1 in HX.
-  rewrite second_id, id_right in HX.
-  unfold split at 1 in HY.
-  rewrite second_id, id_right in HY.
-  simpl.
+  assert ((split mappend[X] mappend[Y] ∘ toggle ∘ exl) △ (id[x × y] ∘ exr)
+            ≈ split (split mappend[X] mappend[Y] ∘ toggle) id[x × y])
+    by (unfork; cat).
+  rewrite X0; clear X0.
+  assert ((id[x × y] ∘ exl) △ (split mappend[X] mappend[Y] ∘ toggle ∘ exr)
+            ≈ split id[x × y] (split mappend[X] mappend[Y] ∘ toggle))
+    by (unfork; cat).
+  rewrite X0; clear X0.
   unfold toggle.
   rewrite !split_fork.
-  rewrite <- !(split_fork _ _ exl exr).
-  rewrite !fork_exl_exr, !id_right.
+  rewrite <- !fork_comp.
   rewrite <- !comp_assoc.
   rewrite !split_fork.
-  rewrite <- fork_comp.
-  unfold split.
-  rewrite !second_id, !id_right.
-  rewrite id_left.
-  rewrite !first_second.
-  rewrite <- !comp_assoc.
-  rewrite <- !first_comp.
-  rewrite exl_fork.
-  rewrite <- first_second.
-  rewrite first_comp.
+  rewrite !id_left.
   rewrite !comp_assoc.
-  rewrite HX; clear HX.
-  rewrite exr_fork.
+  rewrite !exl_fork.
+  rewrite !exr_fork.
   rewrite <- !comp_assoc.
-  rewrite <- !first_second.
-  rewrite !first_comp.
-  rewrite !comp_assoc.
-  rewrite HY; clear HY.
-  rewrite <- !comp_assoc.
-  rewrite <- !split_fork.
-  comp_left.
-  rewrite !comp_assoc.
   rewrite !split_comp.
-  rewrite !split_fork.
-  rewrite <- !comp_assoc.
-  rewrite <- !fork_comp.
-  rewrite <- !comp_assoc.
-  rewrite <- !fork_comp.
-  unfold split.
-  rewrite <- !comp_assoc.
-  rewrite !second_fork.
-  rewrite !first_fork.
-  rewrite !second_fork.
-  rewrite !first_fork.
+  rewrite !exl_fork.
+  rewrite !exr_fork.
+  rewrite !id_right.
+  rewrite <- (id_left (exl (x:=x) (y:=y))) at 1.
+  rewrite <- (id_left (exr (x:=x) (y:=y))) at 1.
+  rewrite <- !split_comp.
   rewrite !comp_assoc.
-  rewrite !exl_fork, !exr_fork.
-  rewrite <- !comp_assoc.
-  rewrite !second_fork.
-  rewrite !first_fork.
-  rewrite !comp_assoc.
-  apply fork_respects; apply fork_respects;
-  unfold first, second;
-  now unfork.
+  rewrite HX, HY.
+  rewrite !id_left.
+  apply fork_respects; clear;
+  now unfold split; unfork; cat.
 Qed.
 
 End Monoid.
