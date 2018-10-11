d4c05058fce48134fddc19eec92b7963a4883e9f
John Wiegley <johnw@newartisans.com>
2018-10-11 15:54:08 -0700

    More work in Lambda.v

--- Instance/Lambda.v
+++ Instance/Lambda.v
@@ -51,7 +51,7 @@ Notation "% x" := (Var x) (at level 20).
 Infix "@" := App (at level 25, left associativity).
 Notation "\ A => M" := (Abs A M) (at level 35).
 
-Reserved Notation "E |- Pt := A" (at level 60).
+Reserved Notation "E |- t := A" (at level 60).
 
 Require Import Coq.Lists.List.
 
@@ -120,9 +120,7 @@ Qed.
 
 Definition option_dec {A} (H : ∀ a b : A, {a = b} + {a <> b}) :
   ∀ a b : option A, {a = b} + {a <> b}.
-Proof.
-  decide equality.
-Defined.
+Proof. decide equality. Defined.
 
 Lemma typing_unique : ∀ E t T (D1 D2 : Typing E t T), D1 = D2.
 Proof.
@@ -152,12 +150,118 @@ Proof.
   destruct M, N; simpl in *; subst.
   pose proof (Type_unique _ _ _ _ typing0 typing1); subst.
   f_equal.
-  apply typing_unique.
+  now apply typing_unique.
 Qed.
+
+Theorem autoType E t :
+  { N : term | env N = E & tm N = t } +
+  { ~ exists N : term, env N = E /\ tm N = t }.
+Proof.
+  induction t.
+  - destruct (nth_error E i) eqn:?.
+      destruct o.
+        left.
+        unshelve eexists
+          {| env    := E
+           ; tm     := %i
+           ; ty     := t
+           ; typing := TVar _ _ _ _ |}; auto.
+      right.
+      intro.
+      destruct H, H; subst.
+      admit.
+    admit.
+  - destruct IHt.
+      left.
+      destruct s; subst.
+      unshelve eexists
+        {| env    := env x
+         ; tm     := \d => tm x
+         ; ty     := d --> ty x
+         ; typing := TAbs _ _ _ (tm x) _ |}; eauto.
+      admit.
+    right.
+    intro.
+    apply n; clear n.
+    destruct H, H; subst.
+    unshelve eexists
+      {| env    := env x
+       ; tm     := t
+       ; ty     := ty x
+       ; typing := _ |}; eauto.
+    admit.
+  - destruct IHt1.
+      destruct IHt2.
+        left.
+        destruct s, s0; subst.
+        unshelve eexists
+          {| env    := env x
+           ; tm     := tm x @ tm x0
+           ; ty     := _
+           ; typing := _ |}; eauto.
+      right.
+Admitted.
+(*
+      destruct s; subst.
+      intro.
+      apply n; clear n.
+      destruct H, H.
+      now eexists
+        {| env    := env x
+         ; tm     := t2
+         ; ty     := _
+         ; typing := _ |}; eauto.
+    destruct IHt2.
+      right.
+      destruct s; subst.
+      intro.
+      apply n; clear n.
+      destruct H, H.
+      now eexists
+        {| env    := env x
+         ; tm     := t1
+         ; ty     := _
+         ; typing := _ |}; eauto.
+    right.
+    intro.
+    apply n; clear n.
+    destruct H, H; subst.
+    now eexists
+      {| env    := env x
+       ; tm     := t1
+       ; ty     := _
+       ; typing := _ |}; eauto.
+}
+*)
+
+Fixpoint subst (z : nat) (u e : preterm) {struct e} : preterm :=
+  match e with
+    | Var i => Var i
+    | Abs e1 => Abs (subst z u e1)
+    | App e1 e2 => App (subst z u e1) (subst z u e2)
+  end.
 
 Program Definition identity (Γ : Env) (x : type) :
   Typing Γ (\x => %0) (x --> x) :=
   TAbs _ _ _ _ (TVar _ _ _ _).
+
+Fixpoint lift (t : preterm) : preterm :=
+  match t with
+  | Var i        => Var (S i)
+  | Abs d body   => Abs d (lift body)
+  | App arg body => App (lift arg) (lift body)
+  end.
+
+Definition lifted (Γ : Env) (x α : type) (t : preterm) :
+  Γ |- t := α -> Γ ∪ {x} |- t := α.
+Proof.
+  intros.
+  induction t.
+  - constructor.
+    inversion H; subst.
+    inversion H2; subst.
+    unfold VarD.
+Abort.
 
 Definition composition (Γ : Env) (x y z : type) (f g : preterm)
            (Tf : Typing Γ f (y --> z))
