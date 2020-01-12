380ff60d34c306f7005babc3dade1d96b5eeb935
John Wiegley <johnw@newartisans.com>
2020-01-12 15:09:07 -0800

    Update to Coq 8.10.2

--- default.nix
+++ default.nix
@@ -1,13 +1,32 @@
 { packages ? "coqPackages_8_10"
 
 , rev      ? "7e8454fb856573967a70f61116e15f879f2e3f6a"
-, sha256   ? "18rcnqxkninl5a560ss39s4nbqf0a677q6qh1fh7i0lr9pxf6aan"
+, sha256   ? "0lnbjjvj0ivpi9pxar0fyk8ggybxv70c5s0hpsqf5d71lzdpxpj8"
 
 , pkgs     ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256; }) {
     config.allowUnfree = true;
     config.allowBroken = false;
+    overlays = [
+      (self: super:
+       let
+         nixpkgs = { rev, sha256 }:
+           import (super.fetchFromGitHub {
+             owner = "NixOS";
+             repo  = "nixpkgs";
+             inherit rev sha256;
+           }) { config.allowUnfree = true; };
+
+         known-good-20191113_070954 = nixpkgs {
+           rev    = "620124b130c9e678b9fe9dd4a98750968b1f749a";
+           sha256 = "0xgy2rn2pxii3axa0d9y4s25lsq7d9ykq30gvg2nzgmdkmy375rr";
+         };
+       in
+       {
+         inherit (known-good-20191113_070954) shared-mime-info;
+       })
+    ];
   }
 }:
 
