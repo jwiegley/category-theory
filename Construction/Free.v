b68c467bf6c1fa738793db30fdeb0bb90e935d1d
John Wiegley <johnw@newartisans.com>
2021-12-02 09:31:14 -0800

    Simplify Free category construction

--- Construction/Free.v
+++ Construction/Free.v
@@ -23,7 +23,22 @@ Context {C : Category}.
    quiver, and the morphisms are paths between objects. Here, a path is
    defined as a finite sequence " *)
 
-Definition composition {x y : C} : tlist hom x y -> x ~{C}~> y.
+Program Definition Free : Category := {|
+  obj     := C;
+  hom     := tlist hom;
+  homset  := fun _ _ =>
+    {| equiv := fun f g => eq f g |};
+  id      := fun _ => tnil;
+  compose := fun _ _ _ f g => g +++ f
+|}.
+Next Obligation. exact eq_equivalence. Qed.
+Next Obligation. now apply tlist_app_tnil_r. Qed.
+Next Obligation. now apply tlist_app_assoc. Qed.
+Next Obligation. symmetry; now apply tlist_app_assoc. Qed.
+
+End Free.
+
+Definition composition {C : Category} {x y : C} : tlist hom x y -> x ~{C}~> y.
 Proof.
   intros.
   induction X.
@@ -31,10 +46,11 @@ Proof.
   exact (compose IHX b).
 Defined.
 
-Definition composition_tnil {x : C} : composition tnil ≈ id[x].
+Definition composition_tnil {C : Category} {x : C} : composition tnil ≈ id[x].
 Proof. now cat. Qed.
 
-Definition composition_tapp {x y z : C} (g : tlist hom y z) (f : tlist hom x y) :
+Definition composition_tapp {C : Category} {x y z : C}
+           (g : tlist hom y z) (f : tlist hom x y) :
   composition (f +++ g) ≈ composition g ∘ composition f.
 Proof.
   induction f; simpl.
@@ -45,23 +61,6 @@ Proof.
   rewrite IHf.
   now cat.
 Qed.
-
-Program Definition Free : Category := {|
-  obj     := C;
-  hom     := tlist hom;
-  homset  := fun _ _ =>
-    {| equiv := fun f g => composition f ≈ composition g |};
-  id      := fun _ => tnil;
-  compose := fun _ _ _ f g => g +++ f
-|}.
-Next Obligation. now equivalence. Qed.
-Next Obligation. now rewrite !composition_tapp, X, X0. Qed.
-Next Obligation. now rewrite tlist_app_tnil_r. Qed.
-Next Obligation. now rewrite tlist_app_tnil_l. Qed.
-Next Obligation. now rewrite tlist_app_assoc. Qed.
-Next Obligation. now rewrite tlist_app_assoc. Qed.
-
-End Free.
 
 Program Definition FreeFunctor : Free ⟶ Cat := {|
   fobj := fun x => x;
