7d0e36a46bc25f874abbc83f9c0be369c7872e8b
John Wiegley <johnw@newartisans.com>
2022-03-28 23:48:06 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
-args@{ coqPackages ? "coqPackages_8_15"
-
-, rev    ? "9222ae36b208d1c6b55d88e10aa68f969b5b5244"
+args@{
+  rev    ? "9222ae36b208d1c6b55d88e10aa68f969b5b5244"
 , sha256 ? "0dvl990alr4bb64w9b32dhzacvchpsspj8p3zqcgk7q5akvqh1mw"
 , pkgs   ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
@@ -10,29 +9,37 @@ args@{ coqPackages ? "coqPackages_8_15"
   }
 }:
 
-with pkgs.${coqPackages};
+let category-theory = coqPackages:
+  with pkgs.${coqPackages}; pkgs.stdenv.mkDerivation rec {
+    name = "coq${coq.coq-version}-category-theory-${version}";
+    version = "1.0";
 
-pkgs.stdenv.mkDerivation rec {
-  name = "coq${coq.coq-version}-category-theory-${version}";
-  version = "1.0";
+    src = if pkgs ? coqFilterSource
+          then pkgs.coqFilterSource [] ./.
+          else ./.;
 
-  src = if pkgs ? coqFilterSource
-        then pkgs.coqFilterSource [] ./.
-        else ./.;
+    buildInputs = [
+      coq coq.ocaml coq.camlp5 coq.findlib equations # coqhammer pkgs.z3-tptp
+    ];
+    enableParallelBuilding = true;
 
-  buildInputs = [
-    coq coq.ocaml coq.camlp5 coq.findlib equations # coqhammer pkgs.z3-tptp
-  ];
-  enableParallelBuilding = true;
+    buildFlags = [
+      "JOBS=$(NIX_BUILD_CORES)"
+    ];
 
-  buildFlags = [
-    "JOBS=$(NIX_BUILD_CORES)"
-  ];
+    installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
 
-  installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
-
-  env = pkgs.buildEnv { inherit name; paths = buildInputs; };
-  passthru = {
-    compatibleCoqVersions = v: builtins.elem v [ "8.10" "8.11" "8.12" "8.13" "8.14" "8.15" ];
+    env = pkgs.buildEnv { inherit name; paths = buildInputs; };
+    passthru = {
+      compatibleCoqVersions = v: builtins.elem v [ "8.10" "8.11" "8.12" "8.13" "8.14" "8.15" ];
+    };
   };
+
+in {
+  category-theory_8_10 = category-theory "coqPackages_8_10";
+  category-theory_8_11 = category-theory "coqPackages_8_11";
+  category-theory_8_12 = category-theory "coqPackages_8_12";
+  category-theory_8_13 = category-theory "coqPackages_8_13";
+  category-theory_8_14 = category-theory "coqPackages_8_14";
+  category-theory_8_15 = category-theory "coqPackages_8_15";
 }
