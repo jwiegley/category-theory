00c1bf3ddd7e4825075e541f838ea9badca61592
John Wiegley <johnw@newartisans.com>
2022-09-02 20:17:00 -0700

    Prove the Yang Baxter equation for braided monoidal categories

--- Structure/Monoidal/Braided.v
+++ Structure/Monoidal/Braided.v
@@ -31,4 +31,42 @@ Class BraidedMonoidal := {
     id ⨂ from braid ∘ tensor_assoc ∘ from braid ⨂ id
 }.
 
+Context `{BraidedMonoidal}.
+
+Theorem Yang_Baxter_equation {a b c : C} :
+  braid ⨂ id[a] ∘ tensor_assoc⁻¹
+    ∘ id[b] ⨂ braid ∘ tensor_assoc
+    ∘ braid ⨂ id[c] ∘ tensor_assoc⁻¹
+    ≈ tensor_assoc⁻¹ ∘ id[c] ⨂ braid
+        ∘ tensor_assoc ∘ braid ⨂ id[b]
+        ∘ tensor_assoc⁻¹ ∘ id[a] ⨂ braid.
+Proof.
+  rewrite <- !comp_assoc.
+  rewrite (@comp_assoc _ _ _ _ _ (id ⨂ braid)).
+  rewrite (@comp_assoc _ _ _ _ _ _ (braid ⨂ id)).
+  rewrite <- hexagon_to_identity.
+  rewrite <- !comp_assoc.
+  rewrite (@comp_assoc _ _ _ _ _ tensor_assoc⁻¹ tensor_assoc).
+  rewrite iso_from_to, id_left.
+  rewrite iso_to_from, id_right.
+  rewrite (@comp_assoc _ _ _ _ _ (id ⨂ braid)).
+  rewrite (@comp_assoc _ _ _ _ _ _ (braid ⨂ id)).
+  rewrite <- hexagon_to_identity.
+  rewrite <- !comp_assoc.
+  rewrite (@comp_assoc _ _ _ _ _ tensor_assoc⁻¹ tensor_assoc).
+  rewrite iso_from_to, id_left.
+  rewrite (@comp_assoc _ _ _ _ _ _ tensor_assoc⁻¹).
+  rewrite iso_to_from, id_left.
+  destruct H0.
+  destruct braid_natural0.
+  simpl in *.
+  specialize (n a _ id _ _ (braid0 b c)).
+  rewrite bimap_id_id in n.
+  rewrite id_right in n.
+  rewrite n.
+  rewrite bimap_id_id.
+  rewrite id_right.
+  reflexivity.
+Qed.
+
 End BraidedMonoidal.
