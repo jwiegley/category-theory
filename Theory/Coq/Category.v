128a23b166c85c6f6520468caf751b372cfdd4a3
John Wiegley <johnw@newartisans.com>
2022-09-06 16:43:09 -0700

    Transfer several definitions into Theory/Coq/Category.v

--- Theory/Coq/Category.v
+++ Theory/Coq/Category.v
@@ -1,5 +1,18 @@
 Require Import Category.Lib.
 Require Import Category.Theory.Category.
+Require Import Category.Theory.Isomorphism.
+Require Import Category.Theory.Functor.
+Require Import Category.Theory.Functor.Endo.
+Require Import Category.Theory.Monad.
+Require Import Category.Theory.Morphisms.
+Require Import Category.Structure.Terminal.
+Require Import Category.Structure.Initial.
+Require Import Category.Structure.Cartesian.
+Require Import Category.Structure.Cartesian.Closed.
+Require Import Category.Structure.Cocartesian.
+Require Import Category.Structure.BiCCC.
+Require Import Category.Structure.Constant.
+Require Import Category.Instance.Sets.
 
 Generalizable All Variables.
 
@@ -44,3 +57,125 @@ Arguments compose {Category}%category_scope {x y z}%object_scope
 
 Notation "f $ x" := (f x) (at level 60, right associativity, only parsing).
 Notation "$ x" := (λ f, f x) (at level 60).
+
+#[export]
+Program Instance Coq_Terminal : @Terminal Coq := {
+  terminal_obj := unit : Type;
+  one := λ _ a, tt
+}.
+Next Obligation.
+  extensionality x0.
+  destruct (f x0), (g x0); reflexivity.
+Qed.
+
+#[export]
+Program Instance Coq_Cartesian : @Cartesian Coq := {
+  product_obj := λ x y, x * y : Type;
+  fork := λ _ _ _ f g x, (f x, g x);
+  exl  := λ _ _ p, fst p;
+  exr  := λ _ _ p, snd p
+}.
+Next Obligation.
+  proper; extensionality x0; subst; simpl; cbv; auto.
+  destruct (h x0); reflexivity.
+Qed.
+
+#[export]
+Program Instance Coq_Closed : @Closed Coq _ := {
+  exponent_obj := Basics.arrow;
+  exp_iso := λ _ _ _, {|
+    to   := {| morphism := λ f a b, f (a, b) |};
+    from := {| morphism := λ f p, f (fst p) (snd p) |}
+  |}
+}.
+Next Obligation.
+  proper; extensionality X0; simplify; now cbv.
+Qed.
+Next Obligation.
+  proper; extensionality X0; simplify; now cbv.
+Qed.
+
+#[export]
+Program Instance Coq_Initial : Initial Coq := {
+  terminal_obj := False;
+  one := λ _ _, False_rect _ _
+}.
+Next Obligation.
+  extensionality x0.
+  contradiction.
+Qed.
+
+#[export]
+Program Instance Coq_Cocartesian : @Cocartesian Coq := {
+  product_obj := sum;
+  fork := λ _ _ _ f g x,
+    match x with
+    | Datatypes.inl v => f v
+    | Datatypes.inr v => g v
+    end;
+  exl  := λ _ _ p, Datatypes.inl p;
+  exr  := λ _ _ p, Datatypes.inr p
+}.
+Next Obligation.
+  split; intros.
+  - split; intros;
+    rewrite H; reflexivity.
+  - extensionality x0.
+    destruct x0; firstorder.
+    + eapply equal_f in a; eauto.
+    + eapply equal_f in b; eauto.
+Qed.
+
+Lemma injectivity_is_monic `(f : x ~> y) :
+  (∀ x y, f x = f y → x = y) ↔ Monic f.
+Proof.
+  split.
+  - intros HA.
+    constructor.
+    autounfold in *; intros ??? HB.
+    simpl in *; intros.
+    extensionality x0.
+    cbv in HB.
+    eapply equal_f in HB; eauto.
+  - intros HA ?? HB.
+    pose (λ (_ : unit), x0) as const_x.
+    pose (λ (_ : unit), y0) as const_y.
+    destruct HA.
+    specialize (monic unit const_x const_y).
+    unfold const_x in monic.
+    unfold const_y in monic.
+    cbv in monic.
+    eapply equal_f in monic; auto.
+    + now rewrite HB.
+    + exact tt.
+Qed.
+
+Lemma surjectivity_is_epic `(f : x ~> y) :
+  (∀ y, exists x, f x = y)%type ↔ Epic f.
+Proof.
+  split.
+  - intros HA.
+    constructor.
+    autounfold in *; intros ??? HB.
+    simpl in *; intros.
+    extensionality x0.
+    specialize (HA x0).
+    destruct HA as [? HA].
+    rewrite <- HA.
+    cbv in HB.
+    eapply equal_f in HB; eauto.
+  - intros HA ?.
+    destruct HA.
+    specialize epic with (z := Prop).
+    specialize epic with (g1 := λ y0, (exists x0, f x0 = y0)%type).
+    simpl in *.
+    specialize epic with (g2 := λ y, True).
+    cbv in epic.
+    eapply equal_f in epic; eauto.
+    + now rewrite epic.
+    + intros.
+      Axiom propositional_extensionality : ∀ P : Prop, P → P = True.
+      extensionality x0.
+      apply propositional_extensionality.
+      now exists x0.
+Qed.
