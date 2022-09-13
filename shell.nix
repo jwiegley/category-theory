37df6eb3e7a805c8f41833c5dd763d55dd38649c
John Wiegley <johnw@newartisans.com>
2022-09-13 11:15:48 -0400

    Use 8.15

--- shell.nix
+++ shell.nix
@@ -1,2 +1,2 @@
-args@{ version ? "category-theory_cur", pkgs ? null }:
+args@{ version ? "category-theory_8_15", pkgs ? null }:
 (import ./default.nix (if pkgs == null then {} else { inherit pkgs; })).${version}
