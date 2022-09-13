11c318b04f682156093dcc0d5669532f01fe1b60
John Wiegley <johnw@newartisans.com>
2022-09-13 11:12:22 -0400

    Start encoding functors in the solver

--- Solver/Denote.v
+++ Solver/Denote.v
@@ -6,54 +6,105 @@ Set Equations With UIP.
 Require Import Category.Lib.
 Require Import Category.Lib.IList.
 Require Import Category.Theory.Category.
+Require Import Category.Theory.Functor.
 Require Import Category.Solver.Expr.
+
+Generalizable All Variables.
 
 Section Denote.
 
-Context `{Arrows}.
+Context `{Functors}.
 
 Import ListNotations.
 
 Open Scope nat_scope.
 
-Definition helper {f} :
+Definition helper_arr `{@Arrows C} {f : nat} :
   (let '(dom, cod) := nth f tys (0, 0)
    in objD dom ~> objD cod)
     → objD (fst (nth f tys (0, 0))) ~>
       objD (snd (nth f tys (0, 0))).
 Proof. destruct (nth f tys (0, 0)); auto. Defined.
 
+Definition helper_fun {F : nat} :
+  (let '(dom, cod) := nth F args (0, 0)
+   in catD dom ⟶ catD cod)
+    → catD (fst (nth F args (0, 0))) ~>
+      catD (snd (nth F args (0, 0))).
+Proof. destruct (nth F args (0, 0)); auto. Defined.
+
 Import EqNotations.
 
-Program Definition lookup_arr (f : nat) dom :
-  option (∃ cod, objD dom ~> objD cod) :=
+Program Definition lookup_arr (cat : nat) (dom : Obj) (f : nat) :
+  option (∃ cod, objD dom ~{catD cat}~> objD cod) :=
+  let arrows := ith cat cat_arrs def_arrs in
   match eq_dec (fst (nth f tys (0, 0))) dom with
   | left H =>
       Some (snd (nth f tys (0, 0));
             rew [fun x => objD x ~> _] H in
-              helper (ith f arrs _))
+              @helper_arr _ arrows f (ith f arrs _))
   | _ => None
   end.
 
-Fixpoint termD_work dom (e : Term) :
-  option (∃ cod, objD dom ~> objD cod) :=
+Program Definition lookup_fun (dom : nat) (F : nat) :
+  option (∃ cod, catD dom ⟶ catD cod) :=
+  match PeanoNat.Nat.eq_dec (fst (nth F args (0, 0))) dom with
+  | left H =>
+      Some (snd (nth F args (0, 0));
+            rew [fun x => catD x ⟶ _] H in
+              @helper_fun F (ith F funs _))
+  | _ => None
+  end.
+Next Obligation.
+  exact Id.
+Defined.
+
+Axiom fobjs_dom :
+  ∀ `(F : C ⟶ D) `{@Objects C} `{@Objects D} d,
+    fobj[F] (objD (fobjs d)) = objD d.
+Axiom fobjs_cod :
+  ∀ `(F : C ⟶ D) `{@Objects C} `{@Objects D} c,
+    fobj[F] (objD c) = objD (fobjs c).
+
+Program Fixpoint termD_work (cat : nat) (dom : Obj) (e : Term) :
+  option (∃ cod, objD dom ~{catD cat}~> objD cod) :=
   match e with
   | Ident => Some (dom; id)
-  | Morph a => lookup_arr a dom
+  | Morph a => lookup_arr cat dom a
   | Comp f g =>
-    match termD_work dom g with
+    match termD_work cat dom g with
     | Some (mid; g) =>
-      match termD_work mid f with
+      match termD_work cat mid f with
       | Some (y; f) => Some (y; f ∘ g)
       | _ => None
       end
     | _ => None
     end
+  | Fmap F d f =>
+    match lookup_fun d F with
+    | Some (c; F) =>
+      match eq_dec c cat with
+      | left H =>
+        match termD_work d (fobjs dom) f with
+        | Some (y; g) => Some (fobjs y; _)
+        | _ => None
+        end
+      | _ => None
+      end
+    | _ => None
+    end
   end.
+Next Obligation.
+  pose (has_objects (Arrows:=ith d cat_arrs def_arrs)).
+  rewrite <- (fobjs_dom F dom).
+  rewrite <- (fobjs_cod F s).
+  apply fmap.
+  apply g.
+Defined.
 
-Definition termD dom cod (e : Term) :
-  option (objD dom ~> objD cod) :=
-  match termD_work dom e with
+Definition termD cat dom cod (e : Term) :
+  option (objD dom ~{catD cat}~> objD cod) :=
+  match termD_work cat dom e with
   | Some (y; f) =>
     match eq_dec y cod with
     | left ecod =>
@@ -67,8 +118,8 @@ Fixpoint exprD (e : Expr) : Type :=
   match e with
   | Top           => True
   | Bottom        => False
-  | Equiv d c f g =>
-    match termD d c f, termD d c g with
+  | Equiv cat d c f g =>
+    match termD cat d c f, termD cat d c g with
     | Some f, Some g => f ≈ g
     | _, _ => False
     end
--- Solver/Expr.v
+++ Solver/Expr.v
@@ -1,10 +1,12 @@
 Require Import Category.Lib.
 Require Import Category.Lib.IList.
 Require Import Category.Theory.Category.
+Require Import Category.Theory.Functor.
+Require Import Category.Instance.Cat.
 
 Definition Obj := nat.
 
-Definition objD' {C : Category} (d : C) (objs : list C) (n : Obj) :=
+Definition objD' {C : Category} (d : C) (objs : list C) (n : Obj) : C :=
   List.nth n objs d.
 
 Definition arrD' {C : Category} (d : C) (objs : list C) '(dom, cod) :=
@@ -25,15 +27,45 @@ Class Arrows (cat : Category) := {
 }.
 #[export] Existing Instance has_objects.
 
+Definition catD' (d : Cat) (cats : list Cat) (n : nat) : Cat :=
+  List.nth n cats d.
+
+Definition funD' (d : Cat) (cats : list Cat) '(dom, cod) :=
+  catD' d cats dom ⟶ catD' d cats cod.
+
+Class Categories := {
+  def_cat : Cat;
+  cats : list Cat;
+  catD := catD' def_cat cats;
+  def_arrs : Arrows def_cat;
+  cat_arrs : ilist (B:=Arrows) cats;
+}.
+
+Class Functors := {
+  has_cats : Categories;
+
+  funD := funD' def_cat cats;
+  args : list (nat * nat);
+  funs : ilist (B:=funD) args;
+
+  (* fobjs : list (nat * nat); *)
+  fobjs : nat → nat;
+
+  (* fobj[F] (objD (fobjs d)) = objD d *)
+  (* fobj[F] (objD c) = objD (fobjs c) *)
+}.
+#[export] Existing Instance has_cats.
+
 Inductive Term : Set :=
   | Ident
   | Morph (a : nat)
-  | Comp (f g : Term).
+  | Comp (f g : Term)
+  | Fmap (F : nat) (d : Obj) (f : Term).
 
 Inductive Expr : Set :=
   | Top
   | Bottom
-  | Equiv (x y : Obj) (f g : Term)
+  | Equiv (cat : nat) (d c : Obj) (f g : Term)
   | And   (p q : Expr)
   | Or    (p q : Expr)
   | Impl  (p q : Expr).
