25d1965c10cc783cbeabc63f2ed0600fdeb40b00
John Wiegley <johnw@newartisans.com>
2022-09-13 22:20:34 -0400

    Syntactic changes

--- Structure/Group/Proofs.v
+++ Structure/Group/Proofs.v
@@ -60,7 +60,8 @@ Proof.
   rewrite <- mempty_right_diagonal.
   rewrite <- (comp_assoc _ ∆grp f_inv).
   rewrite <- (diagonal_natural _ _ f_inv); simpl.
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ (_ ⨂ _) (_ ⨂ _)).
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ (_ ⨂ _) (_ ⨂ _)).
   rewrite <- bimap_comp.
   rewrite <- (comp_assoc _ eliminate _).
   rewrite (unit_terminal (eliminate ∘ f_inv) (eliminate ∘ f)).
@@ -76,26 +77,36 @@ Proof.
   rewrite <- (comp_assoc _ _ f).
   rewrite <- (diagonal_natural _ _ f); simpl.
   rewrite !comp_assoc.
-  rewrite <- bimap_comp; rewrite id_left.
+  rewrite <- bimap_comp;
+  rewrite id_left.
   rewrite <- (id_right f_inv).
   rewrite bimap_comp.
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ (tensor_assoc ⁻¹) _).
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ (tensor_assoc ⁻¹) _).
   rewrite <- from_tensor_assoc_natural.
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ (id ⨂ ∆X) (∆X)).
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ (id ⨂ ∆X) (∆X)).
   rewrite diagonal_tensor_assoc.
-  rewrite <- (comp_assoc tensor_assoc _ _); rewrite (comp_assoc _ tensor_assoc _); rewrite <- (comp_assoc _ (tensor_assoc ⁻¹) _).
-  rewrite iso_from_to; rewrite id_right.
-  rewrite !comp_assoc; rewrite <- 2!(comp_assoc mappend _ _).
-  rewrite <- 2!bimap_comp; rewrite id_right.
+  rewrite <- (comp_assoc tensor_assoc _ _);
+  rewrite (comp_assoc _ tensor_assoc _);
+  rewrite <- (comp_assoc _ (tensor_assoc ⁻¹) _).
+  rewrite iso_from_to;
+  rewrite id_right.
+  rewrite !comp_assoc;
+  rewrite <- 2!(comp_assoc mappend _ _).
+  rewrite <- 2!bimap_comp;
+  rewrite id_right.
   rewrite !comp_assoc.
   (* ... = Ix⁻¹ *)
   rewrite f_left_inverse.
   rewrite (unit_terminal eliminate (eliminate ∘ (inverse[grp] ∘ f))).
-  rewrite !comp_assoc; rewrite <- 2!(comp_assoc _ inverse[grp] f).
+  rewrite !comp_assoc;
+  rewrite <- 2!(comp_assoc _ inverse[grp] f).
   rewrite bimap_comp.
   rewrite <- (comp_assoc _ _ ∆X).
   assert ((inverse[grp] ∘ f) ⨂ (inverse[grp] ∘ f) ≈ map (inverse[grp] ∘ f)) as R
-    by reflexivity; rewrite R; clear R.
+    by reflexivity;
+  rewrite R; clear R.
   rewrite <- (comp_assoc _ _ ∆X).
   rewrite (diagonal_natural _ _ (inverse[grp] ∘ f)).
   simpl.
@@ -128,40 +139,57 @@ Proof.
   rewrite <- bimap_id_id.
   rewrite <- (comp_assoc _ tensor_assoc _).
   rewrite <- to_tensor_assoc_natural.
-  rewrite comp_assoc; rewrite <- (comp_assoc _ (id ⨂ mappend) _).
-  rewrite <- bimap_comp; rewrite id_left.
+  rewrite comp_assoc;
+  rewrite <- (comp_assoc _ (id ⨂ mappend) _).
+  rewrite <- bimap_comp;
+  rewrite id_left.
   (* ... = y⁻¹((x⁻¹x)y) *)
   rewrite mappend_assoc_sym.
   rewrite <- (comp_assoc _ tensor_assoc _).
   rewrite <- to_tensor_assoc_natural.
-  rewrite !comp_assoc; rewrite <- (comp_assoc mappend _ _).
-  rewrite <- bimap_comp; rewrite id_left.
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc mappend _ _).
+  rewrite <- bimap_comp;
+  rewrite id_left.
   rewrite diagonal_braid2.
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ tensor_assoc (tensor_assoc ⁻¹)).
-  rewrite iso_to_from; rewrite id_right.
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ tensor_assoc (tensor_assoc ⁻¹)).
+  rewrite iso_to_from;
+  rewrite id_right.
   rewrite <- (comp_assoc _ (inverse[grp] ⨂ _) _).
-  rewrite <- bimap_comp; rewrite id_right.
+  rewrite <- bimap_comp;
+  rewrite id_right.
   rewrite hexagon_rotated.
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ (inverse[grp] ⨂ braid) _).
-  rewrite <- bimap_comp; rewrite id_right.
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ (inverse[grp] ⨂ braid) _).
+  rewrite <- bimap_comp;
+  rewrite id_right.
   rewrite braid_invol.
   rewrite <- bimap_id_id.
   rewrite <- (comp_assoc _ (tensor_assoc ⁻¹) _).
   rewrite <- from_tensor_assoc_natural.
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ (tensor_assoc ⁻¹) _).
-  rewrite iso_from_to; rewrite id_right.
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ (tensor_assoc ⁻¹) _).
+  rewrite iso_from_to;
+  rewrite id_right.
   rewrite <- (bimap_id_left_right ∆grp ∆grp).
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ tensor_assoc _).
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ tensor_assoc _).
   rewrite <- bimap_id_id.
   rewrite <- to_tensor_assoc_natural.
-  rewrite !comp_assoc; rewrite <- (comp_assoc mappend (inverse[grp] ⨂ _) _).
-  rewrite <- bimap_comp; rewrite id_right.
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc mappend (inverse[grp] ⨂ _) _).
+  rewrite <- bimap_comp;
+  rewrite id_right.
   rewrite <- (comp_assoc _ braid _).
   rewrite <- bimap_braid.
-  rewrite !comp_assoc; rewrite <- (comp_assoc _ ((inverse[grp] ⨂ id) ⨂ id) _).
-  rewrite <- bimap_comp; rewrite id_left.
+  rewrite !comp_assoc;
+  rewrite <- (comp_assoc _ ((inverse[grp] ⨂ id) ⨂ id) _).
+  rewrite <- bimap_comp;
+  rewrite id_left.
   rewrite <- (comp_assoc _ (mappend ⨂ id) _).
-  rewrite <- bimap_comp; rewrite id_left.
+  rewrite <- bimap_comp;
+  rewrite id_left.
   rewrite !comp_assoc.
   (* ... = y⁻¹(Iy) *)
   rewrite left_inverse.
@@ -175,14 +203,18 @@ Proof.
   rewrite unit_left_braid.
   rewrite <- (id_left inverse[grp]).
   rewrite bimap_comp.
-  rewrite comp_assoc; rewrite <- (comp_assoc _ _ tensor_assoc).
+  rewrite comp_assoc;
+  rewrite <- (comp_assoc _ _ tensor_assoc).
   rewrite to_tensor_assoc_natural.
   rewrite comp_assoc.
   rewrite triangle_identity_right.
-  rewrite comp_assoc; rewrite <- (comp_assoc _ _ tensor_assoc).
-  rewrite iso_from_to; rewrite id_right.
+  rewrite comp_assoc;
+  rewrite <- (comp_assoc _ _ tensor_assoc).
+  rewrite iso_from_to;
+  rewrite id_right.
   rewrite <- (comp_assoc _ _ (∆grp ⨂ id[grp])).
-  rewrite <- bimap_comp; rewrite id_right.
+  rewrite <- bimap_comp;
+  rewrite id_right.
   rewrite <- bimap_id_right_left.
   rewrite !comp_assoc.
   rewrite <- (comp_assoc _ unit_right _).
