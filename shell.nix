6bb150a0ade00b7fdd3caf04e683af5fb509b2e5
John Wiegley <johnw@newartisans.com>
2023-09-22 12:32:35 -0700

    Use 8.17

--- shell.nix
+++ shell.nix
@@ -1,2 +1,2 @@
-args@{ version ? "category-theory_cur", pkgs ? null }:
+args@{ version ? "category-theory_8_17", pkgs ? null }:
 (import ./default.nix (if pkgs == null then {} else { inherit pkgs; })).${version}
